4c5b9b4413d16b7b7fc826fa1836f26484e9576b
Kirill Shumilov <brusher.designer@gmail.com>
2016-06-15 11:22:22 +0300

    feat(node-type): add get and findByIndex methods

--- client/components/patch-canvas/node-type/node-type.service.ts
+++ client/components/patch-canvas/node-type/node-type.service.ts
@@ -30,4 +30,33 @@ export class NodeTypeService {
   count() {
     return this._types.length;
   }
+
+  get(index: number) {
+    let i = index;
+    const count = this.count();
+
+    if (i < 0 && count > 0) {
+      i = count + i;
+    }
+
+    if (i > 0 && count > i) {
+      return this._types[index];
+    }
+
+    return null;
+  }
+
+  findById(id: number) {
+    let result = null;
+    const count = this.count();
+
+    for (let i = 0; i < count; i++) {
+      if (this._types[i].id === id) {
+        result = this._types[i];
+        break;
+      }
+    }
+
+    return result;
+  }
 }
