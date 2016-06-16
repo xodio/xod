76ff06d515dc4632e41b6c369e5023c62cf7649f
Kirill Shumilov <brusher.designer@gmail.com>
2016-06-16 19:34:16 +0300

    feat(service-state): add shared interfaces IServiceState and IModel, all ServiceStates (except NodeTypeService) implements with IServiceState

--- client/components/patch-canvas/node/pin/pin.service.ts
+++ client/components/patch-canvas/node/pin/pin.service.ts
@@ -1,19 +1,14 @@
 import {Injectable} from '@angular/core';
 import {PinModel, PinType} from './pin.model.ts';
-
-interface IPinServiceState {
-  pins: Map<number, PinModel>,
-  selected: PinModel,
-  count: number
-}
+import {IServiceState} from '../../../../share/interfaces.ts';
 
 @Injectable()
 export class PinService {
-  private _state: IPinServiceState;
+  private _state: IServiceState;
 
   constructor() {
     this._state = {
-      pins: new Map<number, PinModel>(),
+      models: new Map<number, PinModel>(),
       selected: null,
       count: 0
     };
@@ -21,7 +16,7 @@ export class PinService {
 
   pins() {
     const pins: Array<PinModel> = [];
-    const iterator = this._state.pins.values();
+    const iterator = this._state.models.values();
     let value = iterator.next();
     while(!value.done) {
       pins.push(value.value);
@@ -31,7 +26,7 @@ export class PinService {
   }
 
   pinsIds(nodeId: number): Array<number> {
-    return this.pins().map(pin => pin.pinId);
+    return this.pins().map(pin => pin.id);
   }
 
   pinsOfNode(nodeId: number): Array<PinModel> {
@@ -39,15 +34,15 @@ export class PinService {
   }
 
   inputPinsIds(nodeId: number): Array<number> {
-    return this.pinsOfNode(nodeId).filter(pin => pin.type === PinType.Input).map(pin => pin.pinId);
+    return this.pinsOfNode(nodeId).filter(pin => pin.type === PinType.Input).map(pin => pin.id);
   }
 
   outputPinsIds(nodeId: number): Array<number> {
-    return this.pinsOfNode(nodeId).filter(pin => pin.type === PinType.Output).map(pin => pin.pinId);
+    return this.pinsOfNode(nodeId).filter(pin => pin.type === PinType.Output).map(pin => pin.id);
   }
 
   pin(id: number): PinModel {
-    return this._state.pins.get(id);
+    return this._state.models.get(id);
   }
 
   // TODO: implement pin validation: it should not be selected
@@ -57,11 +52,11 @@ export class PinService {
   }
 
   isSelected(pin: PinModel) {
-    return this._state.selected && pin && this._state.selected.pinId === pin.pinId;
+    return this._state.selected && pin && this._state.selected.id === pin.id;
   }
 
-  resolve(pinId: number) {
-    return this._state.pins[pinId];
+  resolve(id: number) {
+    return this._state.models[id];
   }
 
   reserveId(): number {
@@ -69,7 +64,7 @@ export class PinService {
   }
 
   createPin(pin: PinModel) {
-    this._state.pins.set(pin.pinId, pin);
+    this._state.models.set(pin.id, pin);
     return pin;
   }
 
