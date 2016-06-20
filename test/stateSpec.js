69fcd23b2a66826ad41ad9cb52083f27c1f952af
Maxim Chepel <maxim@amperka.ru>
2016-06-20 17:56:03 +0300

    test(state): sample state test

--- test/stateSpec.js
+++ test/stateSpec.js
@@ -5,6 +5,7 @@ import { initialState } from '../app/state.js';
 describe('Initial state', function() {
   describe('node types', function () {
     it('cound must be > 0', () => {
+      "use strict";
       assert(Object.keys(initialState.nodeTypes).length > 0);
     });
   });
