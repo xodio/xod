76e1da022fa42a7198d295329351862b0e36d78b
Maxim Chepel <maxim@amperka.ru>
2016-06-22 19:18:02 +0300

    refactor(node): immutable => ramda

--- test/reducersSpec.js
+++ test/reducersSpec.js
@@ -2,64 +2,64 @@ import * as Actions from '../app/actions';
 import initialState from '../app/state';
 import { newId, nodes, lastId, copyNode } from '../app/reducers';
 import chai from 'chai';
+import R from 'ramda';
 
 describe('Nodes reducer', () => {
+  const sharedNodesStore = {
+    0: {
+      id: 0,
+      position: {
+        x: 0,
+        y: 100,
+      },
+    },
+  };
+
   describe('while adding node', () => {
+    let nodesStore = null;
+    beforeEach(
+      () => {
+        nodesStore = R.clone(sharedNodesStore);
+      }
+    );
+
     it('should insert node', () => {
-      const oldState = initialState.project.nodes;
-      const state = nodes(oldState, Actions.addNode({
-        id: null,
-        patchId: 1,
-        typeId: 0,
-        props: {
-          label: 'node',
-        },
-      }));
+      const oldState = nodesStore;
+      const state = nodes(oldState, Actions.addNode({}));
       chai.assert(newId(oldState) + 1 === newId(state));
     });
 
     it('should set appropriate id for a new node', () => {
-      const props = {
-        label: 'node',
-      };
-      const oldState = initialState.project.nodes;
-      const state = nodes(oldState, Actions.addNode({
-        id: null,
-        patchId: 1,
-        typeId: 0,
-        props,
-      }));
+      const state = nodes(nodesStore, Actions.addNode({}));
       const newNode = state[lastId(state)];
       chai.assert(newNode.id === lastId(state));
     });
 
     it('should be reverse operation for node deletion', () => {
       let state = null;
-      const props = {
-        label: 'node',
-      };
-      const oldState = initialState.project.nodes;
-      state = nodes(oldState, Actions.addNode({
-        id: null,
-        patchId: 1,
-        typeId: 0,
-        props,
-      }));
+      state = nodes(nodesStore, Actions.addNode({}));
       state = nodes(state, Actions.deleteNode(lastId(state)));
-      chai.expect(state).to.deep.equal(oldState);
+      chai.expect(state).to.deep.equal(nodesStore);
     });
   });
 
   describe('while removing node', () => {
+    let nodesStore = null;
+    beforeEach(
+      () => {
+        nodesStore = R.clone(sharedNodesStore);
+      }
+    );
+
     it('should remove node', () => {
-      const oldState = initialState.project.nodes;
+      const oldState = nodesStore;
       const state = nodes(oldState, Actions.deleteNode(lastId(oldState)));
 
       chai.assert(lastId(oldState) - 1 === lastId(state));
     });
 
     it('should remove node with specified id', () => {
-      const oldState = initialState.project.nodes;
+      const oldState = nodesStore;
       const removingNodeId = lastId(oldState);
       const state = nodes(oldState, Actions.deleteNode(removingNodeId));
 
@@ -68,18 +68,32 @@ describe('Nodes reducer', () => {
 
     it('should be reverse operation for node insertion', () => {
       let state = null;
-      const oldState = initialState.project.nodes;
-      const removingNodeId = lastId(oldState);
-      const removingNode = copyNode(oldState[removingNodeId]);
-      state = nodes(oldState, Actions.deleteNode(removingNodeId));
+      const removingNodeId = lastId(nodesStore);
+      const removingNode = copyNode(nodesStore[removingNodeId]);
+      state = nodes(nodesStore, Actions.deleteNode(removingNodeId));
       state = nodes(state, Actions.addNode(removingNode));
-      chai.expect(state).to.deep.equal(oldState);
+      chai.expect(state).to.deep.equal(nodesStore);
+    });
+
+    it('should not affect other nodes', () => {
+      const oldState = nodesStore;
+      const removingNodeId = lastId(oldState);
+      const state = nodes(oldState, Actions.deleteNode(removingNodeId));
+
+      chai.assert(!state.hasOwnProperty(removingNodeId));
     });
   });
 
   describe('while moving node', () => {
+    let nodesStore = null;
+    beforeEach(
+      () => {
+        nodesStore = R.clone(sharedNodesStore);
+      }
+    );
+
     it('should move node', () => {
-      const oldState = initialState.project.nodes;
+      const oldState = nodesStore;
       const position = {
         x: 0,
         y: 100,
@@ -89,14 +103,6 @@ describe('Nodes reducer', () => {
       const movedNode = state[lastId(oldState)];
 
       chai.expect(movedNode.position).to.deep.equal(position);
-    });
-
-    it('should not affect other nodes', () => {
-      const oldState = initialState.project.nodes;
-      const removingNodeId = lastId(oldState);
-      const state = nodes(oldState, Actions.deleteNode(removingNodeId));
-
-      chai.assert(!state.hasOwnProperty(removingNodeId));
     });
   });
 });
