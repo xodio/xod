6d8bcd561d9345ff1df592816188eed68f447715
Kirill Shumilov <brusher.designer@gmail.com>
2016-06-22 18:29:24 +0300

    feat(store): create stores and pass their states into app container

--- app/index.jsx
+++ app/index.jsx
@@ -2,7 +2,25 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import App from './containers/App';
+import { Editor, Project, NodeTypes } from './reducers/';
 import initialState from './state';
 
-ReactDOM.render(<App {...initialState} />, document.getElementById('root'));
+const Stores = {
+  Project: createStore(Project),
+  Editor: createStore(Editor),
+  NodeTypes: createStore(NodeTypes),
+};
+
+const projectName = initialState.project.name;
+
+ReactDOM.render(
+  <App
+    name={projectName}
+    project={Stores.Project.getState()}
+    {...Stores.Editor.getState()}
+    {...Stores.NodeTypes.getState()}
+  />,
+  document.getElementById('root')
+);
