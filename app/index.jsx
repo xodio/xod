59b6f2e83c3c0628f0e3f895a7275a125bd34370
Maxim Chepel <maxim@amperka.ru>
2016-06-22 13:29:52 +0300

    fix(app): removed .jsx from App module import declaration

--- app/index.jsx
+++ app/index.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App.jsx';
+import App from './containers/App';
 import { initialState } from './state';
 
 ReactDOM.render(<App data={initialState} />, document.getElementById('root'));
