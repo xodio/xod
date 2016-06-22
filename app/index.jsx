a3e09a52dd6d56def491dc5b20b624c070cb65d7
Kirill Shumilov <brusher.designer@gmail.com>
2016-06-22 11:37:31 +0300

    refactor(initial-state): move initial state into another file

--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
-import initialState from './state';
+import initialState from './shared/initial-state'
 
 ReactDOM.render(<App {...initialState} />, document.getElementById('root'));
