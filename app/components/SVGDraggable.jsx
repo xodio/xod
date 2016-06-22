1e76c17a293e76a95f64b2f8e9e80995152abf8f
Kirill Shumilov <brusher.designer@gmail.com>
2016-06-22 20:53:37 +0300

    feat(drag-n-drop): component moves

--- app/components/SVGDraggable.jsx
+++ app/components/SVGDraggable.jsx
@@ -5,7 +5,7 @@ class SVGDraggable extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = this.getInitialState();
+    this.state = this.getDefaultState();
 
     this.handleDragStart = this.dragStart.bind(this);
     this.handleDragMove = this.dragMove.bind(this);
@@ -14,7 +14,7 @@ class SVGDraggable extends React.Component {
     // console.log('!', this.state.initialPosition);
   }
 
-  getInitialState() {
+  getDefaultState() {
     const state = {
       initialPosition: this.parseChildren(),
       translate: {
@@ -26,12 +26,13 @@ class SVGDraggable extends React.Component {
         y: 0,
       },
       dragged: false,
+      scale: 1,
     };
 
     return state;
   }
 
-  getInitialStateChild(el) {
+  getChildState(el) {
     return {
       element: el,
       x: el.props.x,
@@ -41,40 +42,83 @@ class SVGDraggable extends React.Component {
   parseChildren() {
     let arr = [];
     if ({}.hasOwnProperty.call(this.props.children, 'length')) {
-      arr = R.map(this.getInitialStateChild, this.props.children);
+      arr = R.map(this.getChildState, this.props.children);
     }
 
-    arr.push(this.getInitialStateChild(this.props.children));
+    arr.push(this.getChildState(this.props.children));
 
     return arr;
   }
 
   dragStart(event) {
+    // @TODO: Move component above all other components into this layer!
     this.state.startPosition = {
       x: event.clientX,
       y: event.clientY,
     };
     this.state.dragged = true;
-    console.log('start!', this.state);
+    this.state.scale = 1.1;
+
+    this.forceUpdate();
   }
-  dragMove() {
+  dragMove(event) {
     if (this.state.dragged) {
-      // console.log('move!', arguments);
+      const mousePos = {
+        x: event.clientX,
+        y: event.clientY,
+      };
+
+      this.state.translate = {
+        x: mousePos.x - this.state.startPosition.x,
+        y: mousePos.y - this.state.startPosition.y,
+      };
     }
+
+    this.forceUpdate();
   }
   dragStop() {
-    this.state = this.getInitialState();
-    console.log('stop!', this.state);
+    this.applyTranslate();
+    this.state = this.getDefaultState();
+    this.forceUpdate();
+  }
+
+  applyTranslate() {
+    // @TODO: Apply translate to state!
   }
 
   render() {
+    const styles = {
+      opacity: (this.state.dragged) ? 0.6 : 1,
+    };
+    // @TODO: make a candy from this crap:
+    const dragMonitorProps = (this.state.dragged) ? {
+      x: -500,
+      y: -500,
+      width: 2000,
+      height: 2000,
+      style: {
+        fill: 'transparent',
+      },
+    } : {
+      x: 0,
+      y: 0,
+      width: 0,
+      height: 0,
+      style: {
+        fill: 'transparent',
+      },
+    };
+
     return (
       <g
         onMouseDown={this.handleDragStart}
         onMouseMove={this.handleDragMove}
         onMouseUp={this.handleDragStop}
+        transform={`translate(${this.state.translate.x} ${this.state.translate.y})`}
+        style={styles}
       >
         {this.props.children}
+        <rect {...dragMonitorProps} />
       </g>
     );
   }
