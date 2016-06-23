2412c307f44b9909d04d31c3189ba83fd96ef472
Kirill Shumilov <brusher.designer@gmail.com>
2016-06-23 11:27:11 +0300

    refactor(containers): move creating stores into App

--- app/index.jsx
+++ app/index.jsx
@@ -2,25 +2,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import App from './containers/App';
-import { Editor, Project, NodeTypes } from './reducers/';
-import initialState from './state';
 
-const Stores = {
-  Project: createStore(Project),
-  Editor: createStore(Editor),
-  NodeTypes: createStore(NodeTypes),
-};
-
-const projectName = initialState.project.name;
-
-ReactDOM.render(
-  <App
-    name={projectName}
-    project={Stores.Project.getState()}
-    {...Stores.Editor.getState()}
-    {...Stores.NodeTypes.getState()}
-  />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
