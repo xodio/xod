5bdfaf7e791ccda85290c7533f18f8fef57b8601
Kirill Shumilov <brusher.designer@gmail.com>
2016-06-23 14:13:00 +0300

    refactor(eslint): fix eslint errors

--- app/reducers/editor.js
+++ app/reducers/editor.js
@@ -1,7 +1,7 @@
 import initialState from '../state';
 
 export const editor = (state, action) => {
-  let newState = (state === undefined) ? initialState.editor : state;
+  const newState = (state === undefined) ? initialState.editor : state;
   switch (action.type) {
     default:
       return newState;
--- app/reducers/nodetypes.js
+++ app/reducers/nodetypes.js
@@ -1,7 +1,7 @@
 import initialState from '../state';
 
 export const nodeTypes = (state, action) => {
-  let newState = (state === undefined) ? initialState.nodeTypes : state;
+  const newState = (state === undefined) ? initialState.nodeTypes : state;
   switch (action.type) {
     default:
       return newState;
