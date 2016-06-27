89c91b27db1ce63fa4868141c660e798f999c11e
Kirill Shumilov <brusher.designer@gmail.com>
2016-06-27 19:26:02 +0300

    feat(package): add redux-thunk package as middleware

--- app/middlewares.js
+++ app/middlewares.js
@@ -1,6 +1,8 @@
-import { compose } from 'redux';
+import { compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 export const EditorMiddleware = compose(
+  applyMiddleware(thunk),
   typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ?
     window.devToolsExtension() : f => f
 );
