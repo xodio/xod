e1fbcc3708f7aa699f0fba10329f1e13fdbb8b74
Kirill Shumilov <brusher.designer@gmail.com>
2016-07-08 11:34:12 +0300

    fix(patch): fix mistaken roll back

--- app/containers/Patch.jsx
+++ app/containers/Patch.jsx
@@ -86,7 +86,7 @@ class Patch extends React.Component {
   onNodeRendered(id, props) {
     if (id === 0) return;
 
-    if (!Object.hasOwnProperty.call(this.nodesViewstate, id)) {
+    if (!this.nodesViewstate.hasOwnProperty(id)) {
       this.nodesViewstate[id] = {};
     }
 
