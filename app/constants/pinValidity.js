b88f3a07122f8d70e4a6c1987fffcaffe033eacf
Kirill Shumilov <brusher.designer@gmail.com>
2016-07-08 16:25:26 +0300

    feat(pinSelector): add two selectors: getFullPinsData and getValidPins

---
+++ app/constants/pinValidity.js
@@ -0,0 +1,3 @@
+export const INVALID = 0;
+export const ALMOST = 1;
+export const VALID = 2;
--- app/selectors/pin.js
+++ app/selectors/pin.js
@@ -1,4 +1,8 @@
 import R from 'ramda';
+import { createSelector } from 'reselect';
+import { getNodes } from './node';
+import { getNodeTypes, getPinsByNodeTypeId } from './nodetype';
+import * as PIN_VALIDITY from '../constants/pinValidity';
 
 export const getPins = (state) => R.pipe(
   R.prop('pins')
@@ -20,3 +24,44 @@ export const getPinsByIds = (state, props) => R.pipe(
     return result;
   }, {})
 )(state, props);
+
+export const getFullPinsData = createSelector(
+  [getPins, getNodeTypes, getNodes],
+  (pins, nodeTypes, nodes) => R.pipe(
+    R.values,
+    R.reduce((p, pin) => {
+      const node = nodes[pin.nodeId];
+      const nodeTypePins = getPinsByNodeTypeId({ nodeTypes }, node.typeId);
+      const originalPin = nodeTypePins[pin.key];
+
+      return R.assoc(pin.id, R.merge(pin, originalPin), p);
+    }, {})
+  )(pins)
+);
+
+export const getValidPins = (pins, forPinId) => {
+  const oPin = pins[forPinId];
+
+  return R.pipe(
+    R.values,
+    R.reduce((p, pin) => {
+      const samePin = (pin.id === oPin.id);
+      const sameDirection = (pin.direction === oPin.direction);
+      const sameType = (pin.type === oPin.type);
+
+      let validness = PIN_VALIDITY.INVALID;
+
+      if (!samePin) {
+        if (!sameDirection) { validness = PIN_VALIDITY.ALMOST; }
+        if (sameType) { validness = PIN_VALIDITY.VALID; }
+      }
+
+      const result = {
+        id: pin.id,
+        validness,
+      };
+
+      return R.assoc(pin.id, result, p);
+    }, {})
+  )(pins);
+};
