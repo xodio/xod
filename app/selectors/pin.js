b3e6739ee4cfc5b3a2929e46b8e495c877ed6fa4
Kirill Shumilov <brusher.designer@gmail.com>
2016-07-08 19:35:44 +0300

    refactor(pinSelector): extract some conditions into independent selectors

--- app/selectors/pin.js
+++ app/selectors/pin.js
@@ -38,6 +38,21 @@ export const getFullPinsData = createSelector(
   )(pins)
 );
 
+export const isPinHaveLinks = (pin, links) => R.pipe(
+  R.values,
+  R.filter((link) => (link.fromPinId === pin.id || link.toPinId === pin.id)),
+  R.length,
+  R.flip(R.gt)(0)
+)(links);
+
+export const isPinCanHaveMoreLinks = (pin, links) => (
+  (
+    pin.direction === PIN_DIRECTION.INPUT &&
+    !isPinHaveLinks(pin, links)
+  ) ||
+  pin.direction === PIN_DIRECTION.OUTPUT
+);
+
 export const getValidPins = (pins, links, forPinId) => {
   const oPin = pins[forPinId];
   return R.pipe(
@@ -47,17 +62,7 @@ export const getValidPins = (pins, links, forPinId) => {
       const sameNode = (pin.nodeId === oPin.nodeId);
       const sameDirection = (pin.direction === oPin.direction);
       const sameType = (pin.type === oPin.type);
-      const pinLinks = R.pipe(
-        R.values,
-        R.filter((link) => (link.fromPinId === pin.id || link.toPinId === pin.id))
-      )(links);
-      const canHaveLink = (
-        (
-          pin.direction === PIN_DIRECTION.INPUT &&
-          pinLinks.length === 0
-        ) ||
-        pin.direction === PIN_DIRECTION.OUTPUT
-      );
+      const canHaveLink = isPinCanHaveMoreLinks(pin, links);
 
       let validness = PIN_VALIDITY.INVALID;
 
