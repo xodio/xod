1d3e5cba64246327157db61c7198d0328bb51221
Kirill Shumilov <brusher.designer@gmail.com>
2016-07-11 12:37:35 +0300

    feat(linksReducer): add LINK_ADD validation into linkReducer

--- app/reducers/links.js
+++ app/reducers/links.js
@@ -1,5 +1,6 @@
 import { LINK_ADD, LINK_DELETE } from '../actionTypes';
 import R from 'ramda';
+import Selectors from '../selectors';
 
 const nodeIds = (links) =>
   R.map(link => parseInt(link.id, 10))(R.values(links));
@@ -14,17 +15,33 @@ export const newId = (links) => lastId(links) + 1;
 
 export const copyLink = (link) => R.clone(link);
 
-export const links = (state = {}, action) => {
+export const links = (state = {}, action, context) => {
   let newLink = null;
 
   switch (action.type) {
-    case LINK_ADD:
+    case LINK_ADD: {
+      const pins = Selectors.Pin.getFullPinsData(context);
+      const linksState = Selectors.Link.getGlobalLinks(context);
+      const fromPin = pins[action.payload.fromPinId];
+      const toPin = pins[action.payload.toPinId];
+
+      const linkCanBeCreated = (
+        fromPin.direction !== toPin.direction &&
+        Selectors.Pin.isPinCanHaveMoreLinks(fromPin, linksState) &&
+        Selectors.Pin.isPinCanHaveMoreLinks(toPin, linksState)
+      );
+
+      if (!linkCanBeCreated) {
+        throw new Error('Link cannot been created');
+      }
+
       newLink = {
         fromPinId: action.payload.fromPinId,
         toPinId: action.payload.toPinId,
       };
       newLink.id = newId(state);
       return R.set(R.lensProp(newLink.id), newLink, state);
+    }
     case LINK_DELETE:
       return R.omit([action.payload.id.toString()], state);
     default:
