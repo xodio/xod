5aef1abac82a9d4f7e37e547acbec0c94b5e39ba
Kirill Shumilov <brusher.designer@gmail.com>
2016-07-11 14:48:15 +0300

    feat(middleware): add crashReporter

--- app/middlewares.js
+++ app/middlewares.js
@@ -1,6 +1,17 @@
 import { compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { showError } from './actions';
+import DevTools from './containers/DevTools';
+
+const crashReporter = store => next => action => {
+  try {
+    return next(action);
+  } catch (err) {
+    return store.dispatch(showError(err));
+  }
+};
 
 export const EditorMiddleware = compose(
+  applyMiddleware(crashReporter, thunk),
   DevTools.instrument()
 );
