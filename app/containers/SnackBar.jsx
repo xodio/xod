d0e3141eb8daff4bca96955de80aca51980e1d09
Kirill Shumilov <brusher.designer@gmail.com>
2016-07-11 11:05:26 +0300

    feat(SnackBar): improve SnackBar

--- app/containers/SnackBar.jsx
+++ app/containers/SnackBar.jsx
@@ -1,13 +1,55 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import Selectors from '../selectors';
+import { hideError } from '../actions';
 
-class SnackBar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return <div>SnackBar</div>;
-  }
-}
+const styles = {
+  container: {
+    position: 'fixed',
+    bottom: 0,
+    right: 0,
+    width: '200px',
+  },
+  error: {
+    display: 'block',
+    marginTop: '1px',
+    padding: '8px 16px',
+    color: '#fff',
+    background: '#600',
+    borderBottom: '1px solid #400',
+  },
+};
 
-export default connect(state => state)(SnackBar);
+const SnackBar = ({ errors, actions }) => {
+  const onClick = (timestamp) => actions.hideError.bind(undefined, timestamp);
+
+  return (
+    <ul style={styles.container}>
+      {errors.map((error, i) =>
+        <li
+          key={i}
+          onClick={onClick(error.timestamp)}
+          style={styles.error}
+        >
+          {error.timestamp}:<br />
+          {error.message}
+        </li>
+      )}
+    </ul>
+  );
+};
+SnackBar.propTypes = {
+  errors: React.PropTypes.array,
+  actions: React.PropTypes.object,
+};
+
+const mapStateToProps = (state) => ({
+  errors: Selectors.Errors.getErrors(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators({ hideError }, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SnackBar);
