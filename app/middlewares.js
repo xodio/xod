766daef78d3e1de381c52de60e45b942d95c3788
Kirill Shumilov <brusher.designer@gmail.com>
2016-07-11 14:47:26 +0300

    refactor(redux-devtools): replace buggy DevToolsExtension with redux-devtools

--- app/middlewares.js
+++ app/middlewares.js
@@ -2,7 +2,5 @@ import { compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 export const EditorMiddleware = compose(
-  applyMiddleware(thunk),
-  typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ?
-    window.devToolsExtension() : f => f
+  DevTools.instrument()
 );
