e716859df80144cb05786121466ad303338a4b79
Kirill Shumilov <brusher.designer@gmail.com>
2016-07-14 16:30:45 +0300

    feat(toolbar): stylized toolbat with an inline styles

--- app/containers/Toolbar.jsx
+++ app/containers/Toolbar.jsx
@@ -4,6 +4,35 @@ import { bindActionCreators } from 'redux';
 import Selectors from '../selectors';
 import * as Actions from '../actions';
 
+const styles = {
+  toolbar: {
+    position: 'relative',
+    display: 'block',
+    background: '#1E2329',
+    color: '#fff',
+  },
+  toolbar__logo: {
+    display: 'inline-block',
+    padding: '5px 10px',
+    color: '#7A838F',
+  },
+  projectMeta: {
+    display: 'inline-block',
+    padding: '5px 10px',
+    margin: '0 5px',
+  },
+  'toolbar__upload-button': {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    display: 'block',
+    background: '#7E6403',
+    color: '#fff',
+    padding: '8px 18px 9px',
+    border: 0,
+  },
+};
+
 class Toolbar extends React.Component {
   constructor(props) {
     super(props);
@@ -18,11 +47,11 @@ class Toolbar extends React.Component {
     const meta = this.props.meta;
 
     return (
-      <div className="toolbar">
-        <div className="toolbar__logo">
+      <div className="toolbar" style={styles.toolbar}>
+        <div className="toolbar__logo" style={styles.toolbar__logo}>
           XOD
         </div>
-        <div className="projectMeta">
+        <div className="projectMeta" style={styles.projectMeta}>
           <span
             className="projectMeta__name"
             onClick={this.onProjectNameClick}
@@ -37,6 +66,7 @@ class Toolbar extends React.Component {
         </div>
         <button
           className="toolbar__upload-button"
+          style={styles['toolbar__upload-button']}
           onClick={this.props.onUpload}
         >
           UPLOAD
