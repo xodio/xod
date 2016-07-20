c0ef685d92baedcc1aebbf7390703e0106993431
Victor Nakoryakov <victor@amperka.ru>
2016-07-20 17:47:05 +0300

    fix(tests): bring mocha-webpack back

---
+++ webpack/test.js
@@ -0,0 +1,24 @@
+
+const validate = require('webpack-validator');
+const merge = require('webpack-merge');
+
+const baseConfig = require('./base.js');
+
+const config = merge.smart(baseConfig, {
+  module: {
+    loaders: [
+      {
+        test: /test\/.*\.js$/,
+        loaders: [
+          'babel?presets[]=es2015',
+        ],
+      },
+      {
+        test: /targets\/.*\.js$/,
+        loader: 'null',
+      },
+    ],
+  },
+});
+
+module.exports = validate(config);
