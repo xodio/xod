4a57fb11cd14a3baa7efda8cf450be5d8bfe4678
Kirill Shumilov <brusher.designer@gmail.com>
2016-07-21 12:44:35 +0300

    fix(styles): revert importing styles inside scripts

--- app/index.jsx
+++ app/index.jsx
@@ -4,4 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 
+import './styles/main.scss';
+
 ReactDOM.render(<App />, document.getElementById('root'));
