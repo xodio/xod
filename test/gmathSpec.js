225e3b357e2ae9ff4aaf144c8db8f5af3b20c4fb
Victor Nakoryakov <victor@amperka.ru>
2016-07-29 16:35:34 +0300

    chore(gmath, test): add few more tests for topological sort

--- test/gmathSpec.js
+++ test/gmathSpec.js
@@ -28,5 +28,20 @@ describe('Graph math', () => {
       const sorted = sortGraph([44, 43, 42], [[42, 43], [43, 44]]);
       expect(sorted).to.be.eql([42, 43, 44]);
     });
+
+    it('should throw error for cycled graph', () => {
+      const sort = () => sortGraph([42, 43, 44], [[42, 43], [43, 42]]);
+      expect(sort).to.throw(Error, /cycle/);
+    });
+
+    it('should sort diamond graph', () => {
+      const sorted = sortGraph([44, 43, 42, 45], [[42, 43], [42, 44], [43, 45], [44, 45]]);
+      expect(sorted).to.be.eql([42, 44, 43, 45]);
+    });
+
+    it('should sort clusters', () => {
+      const sorted = sortGraph([44, 43, 42, 45], [[42, 43], [44, 45]]);
+      expect(sorted).to.be.eql([44, 42, 45, 43]);
+    });
   });
 });
