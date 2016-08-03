f27f32e67c65bfcdfc7fb31fed4ed620781df6b3
Victor Nakoryakov <victor@amperka.ru>
2016-08-03 12:56:24 +0300

    feat(runtime): implement links transforming

--- targets/xod-espruino/transformer.js
+++ targets/xod-espruino/transformer.js
@@ -6,42 +6,100 @@ import R from 'ramda';
   * a shape expected by the runtime.
   */
 export function transform(project) {
-  // :: pin string type name -> JS type object
-  const mapType = R.pipe(
+  const nodes = () => R.propOr({}, 'nodes', project);
+
+  const pins = () => R.propOr({}, 'pins', project);
+  const pinList = R.compose(R.values, pins);
+  const pinById = id => R.propOr({}, id, pins());
+
+  const links = () => R.propOr({}, 'links', project);
+  const linkList = R.compose(R.values, links);
+
+  const nodeTypes = () => R.propOr({}, 'nodeTypes', project);
+  const nodeTypeById = id => R.propOr({}, id, nodeTypes());
+
+  // :: Node -> NodeType
+  const nodeTypeByNode = R.compose(nodeTypeById, R.prop('typeId'));
+
+  // :: NodeType -> {Key: NodeType.Pin}
+  const nodeTypePins = R.propOr({}, 'pins');
+
+  // :: String -> JSType -- converts pin string type name to native JS type object
+  const nativeType = R.pipe(
     R.when(R.equals('pulse'), R.always(Boolean)),
     R.when(R.equals('bool'), R.always(Boolean)),
     R.when(R.equals('number'), R.always(Number)),
     R.when(R.equals('string'), R.always(String))
   );
 
-  // :: Node -> NodeType
-  const nodeTypeOf = node => project.nodeTypes[node.typeId] || {};
+  // :: {Key: NodeType.Pin} -> {Key: NodeType.Pin}
+  const filterByDirection = dir => R.filter(R.propEq('direction', dir));
 
-  // :: NodeType -> Pin map containing only inputs
-  const inputPinsOf = nodeType => R.filter(R.propEq('direction', 'input'))(nodeType.pins);
+  // :: NodeType -> {Key: NodeType.Pin}
+  const inputs = R.compose(filterByDirection('input'), nodeTypePins);
+  const outputs = R.compose(filterByDirection('output'), nodeTypePins);
 
-  // :: NodeType -> inputTypes branch
-  const inputTypesOf = nodeType => R.compose(
-    R.map(pin => mapType(pin.type)),
-    inputPinsOf
-  )(nodeType);
-
-  // :: NodeType -> flat NodeType
-  const transformNodeType = nodeType => R.merge(
-    R.pick(['pure', 'setup', 'evaluate'])(nodeType),
-    {
-      inputTypes: inputTypesOf(nodeType),
-    }
+  // :: NodeType -> {Key: JSType}
+  const inputTypes = R.compose(
+    R.map(pin => nativeType(pin.type)),
+    inputs
   );
 
-  // :: Node -> flat Node
-  const transformNode = node => R.mergeAll([
+  // :: NodeType -> TransformedNodeType
+  const transformedNodeType = nodeType => R.merge(
+    R.pick(['pure', 'setup', 'evaluate'])(nodeType),
+    R.objOf('inputTypes', inputTypes(nodeType))
+  );
+
+  // :: Link -> OutLink
+  const linkDestination = R.compose(
+    R.pick(['nodeId', 'key']),
+    pinById,
+    R.nth(1),
+    R.prop('pins')
+  );
+
+  // :: Pin -> [Link]
+  const outgoingLinks = (pin) => R.filter(
+    R.compose(
+      R.equals(pin.id),
+      R.nth(0),
+      R.prop('pins')
+    )
+  )(linkList());
+
+  // :: NodeType.Pin -> Pin
+  const pinByNodeTypePin = R.curry(
+    (ownerNode, nodeTypePin) => R.compose(
+      R.defaultTo({}),
+      R.find(R.where({
+        nodeId: R.equals(ownerNode.id),
+        key: R.equals(nodeTypePin.key)
+      }))
+    )(pinList())
+  );
+
+  // :: NodeType.Pin -> [OutLink]
+  const nodeTypePinOutLinks = ownerNode => R.compose(
+    R.map(linkDestination),
+    outgoingLinks,
+    pinByNodeTypePin(ownerNode)
+  );
+
+  // :: Node -> [OutLink]
+  const nodeOutLinks = node => R.compose(
+    R.reject(R.isEmpty),
+    R.map(nodeTypePinOutLinks(node)),
+    outputs,
+    nodeTypeByNode
+  )(node);
+
+  // :: Node -> TransformedNode
+  const transformedNode = node => R.mergeAll([
     R.pick(['id'])(node),
-    R.compose(transformNodeType, nodeTypeOf)(node),
-    {
-      outLinks: {},
-    }
+    R.compose(transformedNodeType, nodeTypeByNode)(node),
+    R.objOf('outLinks', nodeOutLinks(node)),
   ]);
 
-  return R.map(transformNode, project.nodes || {});
+  return R.map(transformedNode, nodes());
 }
--- test/xod-espruino/transformerSpec.js
+++ test/xod-espruino/transformerSpec.js
@@ -8,7 +8,7 @@ describe('Transformer', function() {
     expect(nodes).to.be.eql({});
   });
 
-  it('should join node and node type', function() {
+  it('should merge node and node type', function() {
     const nodes = transform({
       nodes: {
         42: {
@@ -49,6 +49,48 @@ describe('Transformer', function() {
         },
         outLinks: {},
       },
+    });
+  });
+
+  it('should merge links', function() {
+    const nodes = transform({
+      nodes: {
+        42: { id: 42, typeId: 777 },
+        43: { id: 43, typeId: 777 },
+      },
+      pins: {
+        421: { id: 421, nodeId: 42, key: 'valueIn' },
+        422: { id: 422, nodeId: 42, key: 'valueOut' },
+        431: { id: 431, nodeId: 43, key: 'valueIn' },
+        432: { id: 432, nodeId: 43, key: 'valueOut' },
+      },
+      links: {
+        1: { id: 1, pins: [422, 431] }
+      },
+      nodeTypes: {
+        777: {
+          id: 777,
+          pins: {
+            valueIn: {
+              direction: 'input',
+              key: 'valueIn',
+              type: 'number',
+            },
+            valueOut: {
+              direction: 'output',
+              key: 'valueOut',
+              type: 'number',
+            },
+          },
+        },
+      },
+    });
+
+    expect(nodes[42].outLinks).to.be.eql({
+      valueOut: [{
+        nodeId: 43,
+        key: 'valueIn',
+      }],
     });
   });
 });
