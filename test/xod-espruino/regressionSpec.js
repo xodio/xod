7f5b61846c47c6a19f5e2e3ee2b18cf418e0231d
Victor Nakoryakov <victor@amperka.ru>
2016-08-04 23:09:41 +0300

    test(runtime): stronger regression test

--- test/xod-espruino/regressionSpec.js
+++ test/xod-espruino/regressionSpec.js
@@ -1,11 +1,22 @@
 
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 import { expect } from 'chai';
+
+import Selectors from 'selectors';
 import transpile from 'xod-espruino/transpiler';
-import exampleState from 'state';
+import initialState from 'state';
+import generateReducers from 'reducers';
+import { addNode } from 'actions';
 
 describe('xod-espruino', () => {
   it('should transpile example initial state to kinda valid code', () => {
-    const code = transpile(exampleState);
-    expect(code).to.match(/var project = {.*};/);
+    const store = createStore(generateReducers([1]), initialState, applyMiddleware(thunk));
+    store.dispatch(addNode(1, {x: 100, y: 100}, 1));
+
+    const projectJSON = Selectors.Project.getProjectJSON(store.getState());
+    const project = JSON.parse(projectJSON);
+    const code = transpile(project);
+    expect(code).to.match(/var project = {.+};/);
   });
 });
