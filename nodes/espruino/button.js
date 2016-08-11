d783d5a0beffbf8dacd355f1f47168b9533aa8dd
Victor Nakoryakov <victor@amperka.ru>
2016-08-11 15:34:39 +0300

    fix(nodes): repeat button signals, debounce input

--- nodes/espruino/button.js
+++ nodes/espruino/button.js
@@ -2,7 +2,11 @@
 module.exports.setup = function(e) {
   var pin = new Pin(e.props.pin);
 
-  setWatch(function(e) {
-    e.fire({state: e.state});
-  }, pin, {edge: 'both'});
+  setWatch(function(evt) {
+    e.fire({ state: evt.state });
+  }, pin, {
+    edge: 'both',
+    repeat: true,
+    debounce: 30
+  });
 };
