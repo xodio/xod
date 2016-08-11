d88b8bac08d10956f66725dd5b4af59b5b837480
Kirill Shumilov <brusher.designer@gmail.com>
2016-08-11 13:00:06 +0300

    feat(propType): add IO_LABEL propType and update nodes meta

--- app/constants/property.js
+++ app/constants/property.js
@@ -2,10 +2,12 @@ export const PROPERTY_TYPE = {
   BOOL: 'bool',
   NUMBER: 'number',
   STRING: 'string',
+  IO_LABEL: 'io_label',
 };
 
 export const PROPERTY_DEFAULT_VALUE = {
   BOOL: false,
   NUMBER: 0,
   STRING: '',
+  IO_LABEL: '',
 };
