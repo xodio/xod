1960e66dae5deefce7a25ea3acefcb1de1e9cdbc
Kirill Shumilov <brusher.designer@gmail.com>
2016-09-06 19:11:13 +0300

    refactor(client/api-helpers): rewrite helpers using ramda

--- xod-client/src/api/helpers.js
+++ xod-client/src/api/helpers.js
@@ -1,15 +1,17 @@
+import R from 'ramda';
 import * as Selectors from 'xod-client/user/selectors';
 import { STATUS } from 'xod-client/utils/constants';
 
-const isSucceeded = (action) => (action.meta && action.meta.status === STATUS.SUCCEEDED);
-const hasResponse = (action) => (action.payload && action.payload.response);
-const completedResponse = (action) => isSucceeded(action) && hasResponse(action);
+const notNil = R.complement(R.isNil);
+const isSucceeded = R.pathEq(['meta', 'status'], STATUS.SUCCEEDED);
+const hasResponse = R.pathSatisfies(notNil, ['payload', 'response']);
+const completedResponse = R.allPass([isSucceeded, hasResponse]);
 const currentUser = (action, state) => (
   hasResponse(action) &&
   action.payload.response.id === Selectors.userId(state)
 );
 
-const isError = (action) => (action.error === true);
+const isError = R.propEq('error', true);
 
 export default {
   isSucceeded,
