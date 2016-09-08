a1bef45400193215df22e4fc95ba052f56f09e29
Kirill Shumilov <brusher.designer@gmail.com>
2016-09-08 17:49:28 +0300

    feat(user-login): add a custom user logining method into api, that returns userdata (userpic and username) and his projects on login

--- xod-server/server/models/user.js
+++ xod-server/server/models/user.js
@@ -1,11 +1,15 @@
+const R = require('ramda');
 const methods = require('../utils/methods');
 
 module.exports = function UserModel(User) {
+  User.validatesUniquenessOf('username', { message: 'User already exists' });
+  User.validatesLengthOf('password', { min: 6, message: { min: 'Password is too short' } });
+  User.validatesUniquenessOf('email', { message: 'Email is not unique' });
+
   methods.filter(
     [
       'findById',
 
-      'login',
       'logout',
       'resetPassword',
       'confirm',
@@ -16,4 +20,49 @@ module.exports = function UserModel(User) {
     ],
     User
   );
+
+  User.signIn = (username, password, send) => {
+    const app = User.app;
+    const Project = app.models.Project;
+
+    User.login(
+      {
+        username,
+        password,
+      },
+      'user',
+      (uErr, usr) => {
+        Project.find({
+          where: { ownerId: usr.userId },
+        }, (pErr, proj) => {
+          const err = (uErr || pErr) ? R.merge(uErr, pErr) : null;
+
+          send(err, {
+            id: usr.id,
+            ttl: usr.ttl,
+            userId: usr.userId,
+            username: usr.user.username,
+            userpic: usr.user.userpic,
+            projects: proj,
+          });
+        });
+      }
+    );
+  };
+
+  User.remoteMethod('signIn', {
+    http: { path: '/login', verb: 'post' },
+    accepts: [
+      { arg: 'username', type: 'string', required: true },
+      { arg: 'password', type: 'string', required: true },
+    ],
+    returns: [
+      { arg: 'id', type: 'string', root: true },
+      { arg: 'ttl', type: 'number', root: true },
+      { arg: 'userId', type: 'string', root: true },
+      { arg: 'username', type: 'string', root: true },
+      { arg: 'userpic', type: 'string', root: true },
+      { arg: 'projects', type: ['object'], root: true },
+    ],
+  });
 };
