9580e13bf7c2038e1fa8db035f53cac1e8dc8b03
Kirill Shumilov <brusher.designer@gmail.com>
2016-09-08 17:48:56 +0300

    feat(project-name): add server-side errors constant

---
+++ xod-server/server/constants/errors.js
@@ -0,0 +1,7 @@
+/* eslint-disable max-len */
+
+module.exports = {
+  project: {
+    validProjectName: 'Project name should contain only latin alphabet, digits, minus or underscore char.',
+  },
+};
