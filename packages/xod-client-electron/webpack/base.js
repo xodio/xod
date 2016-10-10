6e0f15a05e849c31354a544447823bd11bfaaf2b
Kirill Shumilov <brusher.designer@gmail.com>
2016-10-10 13:05:13 +0300

    chore(electron): rename directories and files

--- packages/xod-client-electron/webpack/base.js
+++ packages/xod-client-electron/webpack/base.js
@@ -6,7 +6,7 @@ module.exports = {
   devtool: 'source-map',
   entry: [
     'babel-polyfill',
-    './src/app/index.jsx',
+    './src/view/index.jsx',
   ],
   output: {
     filename: 'bundle.js',
