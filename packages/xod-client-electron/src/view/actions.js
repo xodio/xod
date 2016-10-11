3870bc1bd7ab7fd6d9285fa11d62a2a2571593cf
Kirill Shumilov <brusher.designer@gmail.com>
2016-10-11 18:08:18 +0300

    feat(processes): add actions to process actions ( add, progress, success, fail) and use this actions in the xod-client-electron/actions

--- packages/xod-client-electron/src/view/actions.js
+++ packages/xod-client-electron/src/view/actions.js
@@ -1,17 +1,37 @@
 import { ipcRenderer } from 'electron';
+import { addProcess, progressProcess, successProcess, deleteProcess } from 'xod-client';
+
 import { getWorkspace } from '../settings/selectors';
+import { SAVE_PROJECT } from './actionTypes';
 
-export const saveProject = (json) => (dispatch, getState) => {
+export const saveProject = json => (dispatch, getState) => {
   const workspace = getWorkspace(getState().settings);
+  const type = SAVE_PROJECT;
 
-  ipcRenderer.on('saveProject:process', () => {});
+  const processId = dispatch(addProcess(type));
+
+  ipcRenderer.once('saveProject:process', () => dispatch(
+    progressProcess(
+      processId,
+      type,
+      {
+        message: 'Saving in process',
+        percentage: 0.5,
+      }
+    )
+  ));
   ipcRenderer.once('saveProject:complete', () => {
-    dispatch({
-      type: 'SAVE_COMPLETE',
-    });
+    dispatch(successProcess(processId, type));
+    setTimeout(() => {
+      dispatch(deleteProcess(processId, type));
+    }, 1000);
   });
   ipcRenderer.send('saveProject', {
     json,
     path: workspace,
   });
+};
+
+export default {
+  saveProject,
 };
