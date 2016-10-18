0cf1569e68161755467860d50bc176d9e0bc6f1e
Kirill Shumilov <brusher.designer@gmail.com>
2016-10-18 19:13:17 +0300

    refactor(xod-core): add export of pack and make a little updates in fixtures

--- packages/xod-core/src/utils/index.js
+++ packages/xod-core/src/utils/index.js
@@ -3,5 +3,6 @@ import { v4 } from 'uuid';
 export * from './gmath';
 export * from './ramda';
 export * from './unpack';
+export { default as pack } from './pack';
 
 export const generateId = () => v4();
