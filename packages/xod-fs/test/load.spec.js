e006689522ef46adecef7a94ccadc83a2e24bf93
Kirill Shumilov <brusher.designer@gmail.com>
2016-10-18 21:00:21 +0300

    feat(xod-fs/test): add a combined test: load the project, libraries and pack it into xodball
    it fails because we have a new folderIds -> we should export function from xod-core and import it here to make it works properly

--- packages/xod-fs/test/load.spec.js
+++ packages/xod-fs/test/load.spec.js
@@ -2,12 +2,17 @@ import { expect } from 'chai';
 
 import path from 'path';
 import * as Loader from '../src/load';
+import loadLibs from '../src/loadLibs';
+import { pack } from 'xod-core';
 import unpacked from './fixtures/unpacked.json';
+import xodball from './fixtures/xodball.json';
 
 const tempDir = './fs-temp';
+const libDir = './fixtures';
 
 describe('Loader', () => {
   const workspace = path.resolve(__dirname, tempDir, 'workspace');
+  const libPath = path.resolve(__dirname, libDir);
   const projectPath = 'awesome_project';
 
   it('should return an array of projects in workspace', (done) => {
@@ -29,10 +34,30 @@ describe('Loader', () => {
       .catch(done);
   });
 
-  it('should load whole project and pack it', (done) => {
+  it('should load whole project data that ready to be passed into xod-core/pack', (done) => {
     Loader.loadProject(projectPath, workspace)
       .then(project => {
         expect(project).to.deep.equal(unpacked);
+        done();
+      })
+      .catch(done);
+  });
+
+  it('should load whole project, libs and pack it', (done) => {
+    Loader.loadProject(projectPath, workspace)
+      .then(project => new Promise(resolve => {
+        loadLibs(project[0].content.libs, libPath)
+          .then(libs => {
+            resolve({
+              project,
+              libs,
+            });
+          });
+      }))
+      .then(({ project, libs }) => {
+        const packed = pack(project, libs);
+
+        expect(packed).to.deep.equal(xodball);
         done();
       })
       .catch(done);
