9c5c8119a91ef52d3d437dda6d323baae0373fa6
Kirill Shumilov <brusher.designer@gmail.com>
2016-11-18 18:44:37 +0300

    fix(xod-js/nodejs): fix nodejs launcher

--- packages/xod-js/platform/nodejs/launcher.js
+++ packages/xod-js/platform/nodejs/launcher.js
@@ -1,12 +1,2 @@
-// A2 is set to HIGH after reset for some reason.
-// Force it to shut down with a hack.
 
-function onInit() {
-  digitalWrite('A2', false); // TODO: remove hack
-  project.launch();
-}
-
-if (typeof module !== 'undefined') {
-  // Export some entities for tests
-  module.exports.onInit = onInit;
-}
+project.launch();
