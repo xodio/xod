942257b79955980198524692218344b31977dfde
Victor Nakoryakov <nail.xx@gmail.com>
2016-12-12 18:27:35 +0300

    refactor(infra): clean up make.js

--- make.js
+++ make.js
@@ -1,15 +1,29 @@
-
+#!/usr/bin/env node
+/*
+ * make.js -- CLI tool to run NPM commands on packages taking their
+ * dependency graph into account. Usage example:
+ *
+ * $ ./make.js --tree build xod-client-browser
+ *
+ * Run `./make.js --help` for details
+ */
 const path = require('path');
 const async = require('async');
+const program = require('commander');
 const PackageUtilities = require('lerna/lib/PackageUtilities');
 const RunCommand = require('lerna/lib/commands/RunCommand');
 
-//const packages = PackageUtilities.getPackages(PackageUtilities.getPackagesPath(__dirname));
-//console.log(JSON.stringify(topologicallyBatchPackages(packages), null, 2));
-
-
-// https://github.com/lerna/lerna/issues/342
-// https://github.com/seansfkelley/lerna/commit/4e3e4789a4d627f90d8eb899dd9514818105aa7b
+/*
+ * Sorts Lerna packages in topological order and returns array
+ * of batches which can be runned concurrently.
+ *
+ * The implementation is stolen from:
+ * https://github.com/seansfkelley/lerna/commit/4e3e4789a4d627f90d8eb899dd9514818105aa7b
+ *
+ * It will be possibly merged with Lerna upstream. Watch for:
+ * https://github.com/lerna/lerna/issues/342
+ *
+ */
 function topologicallyBatchPackages(packagesToBatch) {
   // We're going to be chopping stuff out of this array, so copy it.
   const packages = packagesToBatch.slice();
@@ -58,10 +72,13 @@ function topologicallyBatchPackages(packagesToBatch) {
   return batches;
 }
 
-function arrayConcat(a, b) {
-  return a.concat(b);
-}
-
+/*
+ * SortedRunCommand is an upgrade to the straightforward `lerna run`.
+ *
+ * It uses topological sort to order inter-dependent packages properly.
+ * And it allows `withDeps` flag that cause the command to be executed
+ * on all dependencies (recursively) of the scope provided.
+ */
 class SortedRunCommand extends RunCommand {
   initialize(callback) {
     super.initialize((err, result) => {
@@ -74,20 +91,24 @@ class SortedRunCommand extends RunCommand {
     if (this.flags.withDeps) {
       this.logger.info('Including package dependencies');
       const graph = PackageUtilities.getPackageGraph(this.packages);
+      const concat = (a, b) => a.concat(b);
 
-      function packageWithDeps(packageGraphNode) {
-        return packageGraphNode.dependencies
+      const packageWithDeps = packageGraphNode =>
+        packageGraphNode.dependencies
           .map(pkgName => graph.get(pkgName))
           .map(packageWithDeps)
-          .reduce(arrayConcat, []).concat([packageGraphNode]);
-      }
+          .reduce(concat, []).concat([packageGraphNode]);
+
+      const unique =
+        (value, index, self) => 
+          self.map(pkg => pkg.name).indexOf(value.name) === index;
 
       this.packagesWithScript = this.packagesWithScript
         .map(pkg => graph.get(pkg.name))
         .map(packageWithDeps)
-        .reduce(arrayConcat, [])
+        .reduce(concat, [])
         .map(graphNode => graphNode.package)
-        .filter((value, index, self) => self.map(pkg => pkg.name).indexOf(value.name) === index);
+        .filter(unique);
     }
 
     callback(null, true);
@@ -114,11 +135,30 @@ class SortedRunCommand extends RunCommand {
   }
 }
 
-process.env['PATH'] = path.join(__dirname, 'node_modules', '.bin') + ':' + process.env['PATH'];
+/*
+ * Make a simple CLI runner of the SortedRunCommand.
+ */
+program
+  .version('0.0.0')
+  .arguments('<npm-command> [package]')
+  .option('-t, --tree', 'Run command on dependencies first')
+  .action((cmd, pkg) => {
+    const flags = {
+      scope: pkg,
+      withDeps: program.tree,
+    };
 
-const cmd = new SortedRunCommand(['build'], {
-  scope: 'xod-client-browser',
-  withDeps: true,
-});
+    const lernaCommand = new SortedRunCommand([cmd], flags);
 
-cmd.run();
+    // extend path with root-level node_modules to access build tools
+    // binaries
+    process.env['PATH'] = path.join(__dirname, 'node_modules', '.bin') +
+      ':' + process.env['PATH'];
+
+    lernaCommand.run();
+  });
+
+program.parse(process.argv);
+if (program.args.length === 0) {
+  console.log(program.help());
+}
