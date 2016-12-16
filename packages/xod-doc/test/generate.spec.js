9490063c50a96915782e4595403c36c05c76661a
Fedor Kamshilin <hrenli@mail.ru>
2016-12-16 13:40:58 +0300

    feat(xod-doc): fixing code style issue

--- packages/xod-doc/test/generate.spec.js
+++ packages/xod-doc/test/generate.spec.js
@@ -14,31 +14,33 @@ describe('Generate docs', () => {
   const file = chaiFiles.file;
   const dir = chaiFiles.dir;
 
+  const tmpPath = path.join(__dirname, 'tmp');
+
   before((done) => {
-    doc(path.resolve(__dirname, 'tmp'), path.resolve(__dirname, 'layouts'), path.resolve(__dirname, 'xod/core'), { clear: true }).then(() => done());
+    doc(tmpPath, path.resolve(__dirname, 'layouts'), path.resolve(__dirname, 'xod/core'), { clear: true }).then(() => done());
   });
 
-  it('shoult create folder tmp', () => expect(dir(path.resolve(__dirname, 'tmp'))).to.exist);
+  it('shoult create folder tmp', () => expect(dir(tmpPath)).to.exist);
 
-  it('shoult create folder nodes', () => expect(dir(path.resolve(__dirname, 'tmp', 'nodes'))).to.exist);
+  it('shoult create folder nodes', () => expect(dir(path.resolve(tmpPath, 'nodes'))).to.exist);
 
-  it('shoult create index file', () => expect(file(path.resolve(__dirname, 'tmp', 'index.html'))).to.exist);
+  it('shoult create index file', () => expect(file(path.resolve(tmpPath, 'index.html'))).to.exist);
 
   it('should create doc files', () => {
     const files = packed.map(el => (el.link.replace('nodes/', '')));
-    expect(path.resolve(__dirname, 'tmp', 'nodes')).to.be.a.directory().and.include.files(files);
+    expect(path.resolve(tmpPath, 'nodes')).to.be.a.directory().and.include.files(files);
   });
 
   it('files should contain not less than 1 match of patch label', () => {
     packed.forEach(el => {
       const re = new RegExp(`<h1>${el.label}</h1>`);
-      expect(file(path.resolve(__dirname, 'tmp', el.link))).to.match(re);
+      expect(file(path.resolve(tmpPath, el.link))).to.match(re);
     });
   });
   it('index should contain all labels', () => {
     packed.forEach(el => {
       const re = new RegExp(el.label);
-      expect(file(path.resolve(__dirname, 'tmp', 'index.html'))).to.match(re);
+      expect(file(path.resolve(tmpPath, 'index.html'))).to.match(re);
     });
   });
 });
