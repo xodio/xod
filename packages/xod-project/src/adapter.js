cea9204aa8365056158453d65babd0dcf927f15a
Kirill Shumilov <brusher.designer@gmail.com>
2017-01-11 21:42:09 +0300

    feat(xod-project): add converting of links (without test) and refactor part of code and test

--- packages/xod-project/src/adapter.js
+++ packages/xod-project/src/adapter.js
@@ -6,14 +6,17 @@ import { Maybe } from 'ramda-fantasy';
 import * as Project from './project';
 import * as Patch from './patch';
 import * as Node from './node';
+import * as Link from './link';
 
 // utils
-const apOrSkip = R.curry((maybeFn, maybeValue) => {
-  const result = maybeFn.ap(maybeValue);
-  return Maybe.isJust(result) ? result : maybeValue;
+const apOrSkip = R.curry((maybeFn, val) => {
+  const maybeVal = Maybe(val);
+  const result = maybeFn.ap(maybeVal);
+  return Maybe.isJust(result) ? result : maybeVal;
 });
+
 const propValues = key => R.compose(R.values, R.propOr({}, key));
-const arrayToFunction = R.compose(
+const applyArrayOfFunctions = R.compose(
   R.apply(R.compose),
   R.append(R.identity),
   R.values
@@ -35,6 +38,7 @@ const getLabel = R.prop('label');
 const getNodes = propValues('nodes');
 const getLinks = propValues('links');
 
+const addLabel = oldPatch => Maybe(getLabel(oldPatch)).map(Patch.setPatchLabel);
 const assocPatchUnsafe = R.curry(
   (path, patch, project) => R.assocPath(['patches', path], patch, project)
 );
@@ -45,49 +49,136 @@ const assocPatchUnsafe = R.curry(
  * @returns {object}
  */
 export const toV2 = (bundle) => {
+  const nodeIdMap = {};
+  // :: Node -> Node
+  const updateNodeIdMap = R.curry((oldNode, newNode) => {
+    nodeIdMap[oldNode.id] = newNode.id;
+    return newNode;
+  });
   // :: Node -> Function
   const convertNodePins = R.compose(
-    arrayToFunction,
+    applyArrayOfFunctions,
     R.mapObjIndexed((pin, key) => {
       const setCurryPin = Maybe(pin.injected).map(Node.curryPin(key));
       const setPinValue = Maybe(pin.value).map(Node.setPinCurriedValue(key));
       return R.compose(
         R.chain(R.identity),
-        apOrSkip(setPinValue),
-        apOrSkip(setCurryPin),
-        Maybe
+        R.chain(apOrSkip(setPinValue)),
+        apOrSkip(setCurryPin)
       );
     }),
     R.prop('pins')
   );
   // :: Patch -> Function
   const convertNodes = R.compose(
-    arrayToFunction,
-    R.map(oldNode => {
-      const position = oldNode.position;
-      const type = oldNode.typeId;
-
-      return Node.createNode(position, type)
+    applyArrayOfFunctions,
+    R.map(oldNode => Node.createNode(oldNode.position, oldNode.typeId)
+      .map(updateNodeIdMap(oldNode))
       .map(convertNodePins(oldNode))
-      .chain(Patch.assocNode);
-    }),
+      .chain(Patch.assocNode)
+    ),
     getNodes
+  );
+  // :: String -> String
+  const getLinkNodeId = nodeId => nodeIdMap[nodeId];
+  // :: String -> String
+  const getLinkPin = pinKey => Maybe(nodeIdMap[pinKey]).getOrElse(pinKey);
+  // :: Patch -> Function
+  const convertLinks = R.compose(
+    R.map(R.compose(
+      Patch.assocLink,
+      R.converge(
+        Link.createLink,
+        [
+          R.compose(getLinkPin, R.prop('pinKey'), R.head),
+          R.compose(getLinkNodeId, R.prop('nodeId'), R.head),
+          R.compose(getLinkPin, R.prop('pinKey'), R.last),
+          R.compose(getLinkNodeId, R.prop('nodeId'), R.last),
+        ]
+      ),
+      R.prop('pins')
+    )),
+    getLinks
+  );
+  // :: Patch[] -> Patch[]
+  const getPatchesWithLinks = R.curry((newBundle, patches) =>
+    R.map(R.converge(
+      R.compose(
+        R.unnest,
+        R.reduce(R.flip(R.chain))
+      ),
+      [
+        R.compose(
+          Project.getPatchByPath(R.__, newBundle),
+          R.prop('id')
+        ),
+        convertLinks,
+      ]
+    ))(patches)
+  );
+  // :: BundleV1 -> BundleV2 -> BundleV2
+  // -- we can use commented lines when we fix next todo
+  // TODO: Add pins from nodeTypes into patches before add links!
+  const convertLinksInPatches = R.curry(
+    (oldBundle, newBundle) => R.compose(
+      R.reduce(
+        // -- R.flip(R.chain),
+        // -- Maybe.of(newBundle)
+        R.flip(R.call),
+        newBundle
+      ),
+      R.map(R.converge(
+        // -- Project.assocPatch,
+        assocPatchUnsafe,
+        [
+          R.head,
+          R.last,
+        ]
+      )),
+      R.converge(
+        R.zip,
+        [
+          R.map(R.prop('id')),
+          getPatchesWithLinks(newBundle),
+        ]
+      ),
+      mergePatchesAndNodeTypes
+    )(oldBundle)
+  );
+  // :: Patch -> Function
+  const copyImpls = R.compose(
+    R.map(R.assoc('impls')),
+    Maybe,
+    R.when(
+      R.isEmpty,
+      R.always(null)
+    ),
+    R.prop('impl')
+  );
+  // :: Project -> Function
+  const appendAuthor = R.compose(
+    R.map(R.compose(
+      R.over(R.lensProp('authors')),
+      R.append
+    )),
+    Maybe,
+    R.path(['meta', 'author'])
   );
   // :: Project -> Function
   const convertPatches = R.compose(
-    arrayToFunction,
-    R.map(oldPatch => {
-      const addLabel = Maybe(getLabel(oldPatch)).map(Patch.setPatchLabel);
-
-      return Maybe.of(Patch.createPatch())
-        .chain(patch => apOrSkip(addLabel, Maybe.of(patch)))
-        .map(convertNodes(oldPatch))
-        .chain(assocPatchUnsafe(oldPatch.id));
-    }),
+    applyArrayOfFunctions,
+    R.map(oldPatch => Maybe.of(Patch.createPatch())
+      .chain(apOrSkip(addLabel(oldPatch)))
+      .map(convertNodes(oldPatch))
+      .chain(apOrSkip(copyImpls(oldPatch)))
+      .chain(assocPatchUnsafe(oldPatch.id))
+    ),
     mergePatchesAndNodeTypes
   );
 
   return Maybe.of(Project.createProject())
+    .chain(apOrSkip(appendAuthor(bundle)))
     .map(convertPatches(bundle))
+    .map(convertLinksInPatches(bundle))
     .chain(R.identity);
 };
--- packages/xod-project/test/adapter.spec.js
+++ packages/xod-project/test/adapter.spec.js
@@ -13,7 +13,26 @@ describe('Adapters', () => {
         .to.be.an('object')
         .and.have.all.keys('authors', 'description', 'license', 'patches');
     });
-    it('should return new version that have four patches', () => {
+    it('should have an author in authors array', () => {
+      expect(toV2(bundleV1))
+        .to.have.property('authors')
+        .that.contains(bundleV1.meta.author);
+    });
+    it('should return new version that have six patches', () => {
+      const patchKeys = [
+        '@/main',
+        '@/sub/aux',
+        'xod/core/and',
+        'xod/core/inputNumber',
+        'xod/core/pot',
+        'xod/core/led',
+      ];
+      expect(toV2(bundleV1))
+        .to.be.an('object')
+        .that.have.property('patches')
+        .that.have.all.keys(patchKeys);
+    });
+    it('should return new version that have six patches', () => {
       const patchKeys = [
         '@/main',
         '@/sub/aux',
