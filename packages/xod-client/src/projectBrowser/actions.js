908ce6bb4adb41eec5d5af0089cad48aa53582d8
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2017-02-06 12:16:09 +0300

    refactor(client): validate popupId in relevant action creators

--- packages/xod-client/src/projectBrowser/actions.js
+++ packages/xod-client/src/projectBrowser/actions.js
@@ -1,14 +1,33 @@
+import R from 'ramda';
 import * as actionTypes from './actionTypes';
+import { POPUP_ID } from './constants';
 
-export const openPopup = popupId => ({
-  type: actionTypes.PROJECT_BROWSER_OPEN_POPUP,
-  payload: { popupId },
-});
-export const closePopup = popupId => ({
-  type: actionTypes.PROJECT_BROWSER_CLOSE_POPUP,
-  payload: { popupId },
-});
+const validPopupIds = new Set(R.values(POPUP_ID));
+const validatePopupId = (popupId) => {
+  if (!validPopupIds.has(popupId)) {
+    throw new Error(`Unknown popup id ${popupId}`);
+  }
+
+  return popupId;
+};
+
+export const openPopup = R.pipe(
+  validatePopupId,
+  popupId => ({
+    type: actionTypes.PROJECT_BROWSER_OPEN_POPUP,
+    payload: { popupId },
+  })
+);
+export const closePopup = R.pipe(
+  validatePopupId,
+  popupId => ({
+    type: actionTypes.PROJECT_BROWSER_CLOSE_POPUP,
+    payload: { popupId },
+  })
+);
 
 export const closeAllPopups = () => ({
   type: actionTypes.PROJECT_BROWSER_CLOSE_ALL_POPUPS,
 });
+
+export const startCreatingPatch = R.partial(openPopup, [POPUP_ID.CREATING_PATCH]);
