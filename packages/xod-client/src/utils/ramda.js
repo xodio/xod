1e7d8ee7e95b44c405e1b20923e8ab24ac9d9a54
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2017-02-07 18:05:09 +0300

    refactor(client): use compose instead of pipe in propsAreFalsy

--- packages/xod-client/src/utils/ramda.js
+++ packages/xod-client/src/utils/ramda.js
@@ -5,7 +5,7 @@ export const notNil = R.complement(R.isNil);
 export const noop = R.always(undefined);
 
 // :: [propName] -> Object -> Bool
-export const propsAreFalsy = R.curry(R.pipe(
-  R.props,
-  R.all(R.not)
+export const propsAreFalsy = R.curry(R.compose(
+  R.all(R.not),
+  R.props
 ));
