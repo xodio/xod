5b8a704c9772155ff45bd5931a9dbc8f8cec302c
Kirill Shumilov <brusher.designer@gmail.com>
2017-02-14 21:13:43 +0300

    refactor(xod-js): make target-specifed transpiling functions works correctly

--- packages/xod-js/src/target-espruino.js
+++ packages/xod-js/src/target-espruino.js
@@ -2,9 +2,18 @@ import runTranspile from './transpiler';
 
 import espruinoLauncher from '../platform/espruino/launcher';
 
-export default function transpile(project) {
+/**
+ * Runs transpilation for Espruino target.
+ *
+ * @function transpile
+ * @param {Project} project Whole project (v2)
+ * @param {Path} path Path of entry-point patch
+ * @returns {String} Transpiled code
+ */
+export default function transpile(project, path) {
   return runTranspile({
     project,
+    path,
     impls: ['espruino', 'js'],
     launcher: espruinoLauncher,
   });
--- packages/xod-js/src/target-nodejs.js
+++ packages/xod-js/src/target-nodejs.js
@@ -2,9 +2,18 @@ import runTranspile from './transpiler';
 
 import nodejsLauncher from '../platform/nodejs/launcher';
 
-export default function transpile(project) {
+/**
+ * Runs transpilation for NodeJS target.
+ *
+ * @function transpile
+ * @param {Project} project Whole project (v2)
+ * @param {Path} path Path of entry-point patch
+ * @returns {String} Transpiled code
+ */
+export default function transpile(project, path) {
   return runTranspile({
     project,
+    path,
     impls: ['nodejs', 'js'],
     launcher: nodejsLauncher,
   });
