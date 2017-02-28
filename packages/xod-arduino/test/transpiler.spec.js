f12e84e716fe0f3f8ff32edf07c76e7e9fa26a16
Kirill Shumilov <brusher.designer@gmail.com>
2017-02-28 12:37:36 +0300

    chore(xod-arduino): replace redundant code

--- packages/xod-arduino/test/transpiler.spec.js
+++
@@ -1,8 +0,0 @@
-import { assert } from 'chai';
-import transpiler from '../src/transpiler';
-
-describe('xod-arduino', () => {
-  it('package is correctly configured', () => {
-    assert.isTrue(transpiler());
-  });
-});
