a1608cb8666fda5009c176360ea2c89d24a631d7
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2017-02-28 11:54:38 +0300

    refactor(xod-client): add dedicated webpack config for storybook
    instead of borrowing one from `xod-client-browser`

--- packages/xod-client/.storybook/webpack.config.js
+++ packages/xod-client/.storybook/webpack.config.js
@@ -1,5 +1,42 @@
-const R = require('ramda');
-// TODO: although this is very convinient, we create thght coupling with xod-client-browser package
-const clientBrowserConfig = require('../../xod-client-browser/webpack.config');
+const fs = require('fs');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
 
-module.exports = R.omit(['entry', 'output', 'plugins'], clientBrowserConfig);
+const pkgpath = subpath => path.join(__dirname, subpath);
+
+module.exports = {
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style',
+          'css',
+          'postcss',
+          'sass?outputStyle=expanded',
+        ],
+      },
+      {
+        test: /assets\/.*\.(jpe?g|png|gif|svg|ttf|eot|svg|woff|woff2)?$/,
+        loaders: [
+          `file?name=assets/[path][name].[ext]?[hash:6]&context=${pkgpath('./src/core/assets')}`,
+        ],
+      },
+      {
+        test: /node_modules\/font-awesome\/.*\.(jpe?g|png|gif|svg|ttf|eot|svg|woff|woff2)(\?\S*)?$/,
+        loaders: [
+          'file?name=assets/font-awesome/[name].[ext]?[hash:6]',
+        ],
+      },
+      {
+        include: pkgpath('node_modules/font-awesome'),
+        test: /\.css$/,
+        loaders: [
+          'style',
+          'css',
+        ],
+      },
+    ],
+  },
+  postcss: function postCssPlugins() { return [autoprefixer]; },
+};
