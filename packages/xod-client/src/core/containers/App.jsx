2faed8152e37de093695ac11ecfa4edbe79a19ab
Kirill Shumilov <brusher.designer@gmail.com>
2017-03-22 17:58:32 +0300

    refactor(xod-client): make client works properly with Either, that returns from `transpileForArduino` and show code or show error message

--- packages/xod-client/src/core/containers/App.jsx
+++ packages/xod-client/src/core/containers/App.jsx
@@ -2,24 +2,36 @@ import React from 'react';
 import { toV2 } from 'xod-project';
 import { transpileForEspruino, transpileForNodeJS } from 'xod-js';
 import { transpileForArduino } from 'xod-arduino';
+import { foldEither } from 'xod-func-tools';
 
-const showTranspiledCode = (context, transpiler) => {
+const transpile = (context, transpiler) => {
   const projectV2 = toV2(context.props.project);
-  context.setState({
-    code: transpiler(projectV2, context.props.currentPatchId),
-  });
+  return transpiler(projectV2, context.props.currentPatchId);
+};
+
+const showCode = (context, code) => {
+  context.setState({ code });
   context.showCodePopup();
+};
+
+const showError = (context, error) => {
+  context.props.actions.addError(error);
 };
 
 export default class App extends React.Component {
   onShowCodeEspruino() {
-    showTranspiledCode(this, transpileForEspruino);
+    showCode(this, transpile(this, transpileForEspruino));
   }
   onShowCodeNodejs() {
-    showTranspiledCode(this, transpileForNodeJS);
+    showCode(this, transpile(this, transpileForNodeJS));
   }
   onShowCodeArduino() {
-    showTranspiledCode(this, transpileForArduino);
+    const eitherCode = transpile(this, transpileForArduino);
+    foldEither(
+      error => showError(this, error),
+      code => showCode(this, code),
+      eitherCode
+    );
   }
 
   render() {
