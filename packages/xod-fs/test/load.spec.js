90a797443a90638a8bdcf2c2291bea53f0302412
Alexander Matsievsky <am@amperka.ru>
2017-03-29 16:27:36 +0300

    tweak(xod-core): xod-fs should pack/unpack implementations
    Add tests.
    
    Closes #441.

--- packages/xod-fs/test/load.spec.js
+++ packages/xod-fs/test/load.spec.js
@@ -1,13 +1,14 @@
 import { expect } from 'chai';
 
+import fs from 'fs';
 import path from 'path';
+import shell from 'shelljs';
 import * as Loader from '../src/load';
 import pack from '../src/pack';
-
-import { expectEqualToXodball } from './utils';
+import libsFixture from './fixtures/libs.json';
 import unpacked from './fixtures/unpacked.json';
 import xodball from './fixtures/xodball.json';
-import libsFixture from './fixtures/libs.json';
+import { expectEqualToXodball } from './utils';
 
 const tempDir = './fixtures/workspace';
 
@@ -17,33 +18,55 @@ describe('Loader', () => {
 
   it('should return an array of projects in workspace', (done) => {
     Loader.getProjects(workspace)
-      .then((projects) => {
-        expect(projects).to.have.lengthOf(1);
-        expect(projects).to.deep.equal([
-          {
-            meta: {
-              name: 'Awesome project',
-              author: 'Amperka team',
-            },
-            libs: ['xod/core'],
-            path: path.resolve(workspace, projectPath),
-          },
-        ]);
-        done();
-      })
-      .catch(done);
+          .then((projects) => {
+            expect(projects).to.have.lengthOf(1);
+            expect(projects).to.deep.equal([
+              {
+                meta: {
+                  name: 'Awesome project',
+                  author: 'Amperka team',
+                },
+                libs: ['xod/core'],
+                path: path.resolve(workspace, projectPath),
+              },
+            ]);
+            done();
+          })
+          .catch(done);
   });
 
   it('should load whole project, libs and pack it', (done) => {
     Loader.loadProjectWithLibs(projectPath, workspace)
-      .then(({ project, libs }) => {
-        expect(project).to.deep.equal(unpacked);
-        expect(libs).to.deep.equal(libsFixture);
+          .then(({ project, libs }) => {
+            expect(project).to.deep.equal(unpacked);
+            expect(libs).to.deep.equal(libsFixture);
 
-        const packed = pack(project, libs);
-        expectEqualToXodball(packed, xodball, expect);
-        done();
-      })
-      .catch(done);
+            const packed = pack(project, libs);
+            expectEqualToXodball(packed, xodball, expect);
+            done();
+          })
+          .catch(done);
+  });
+
+  it('should load patch implementations', (done) => {
+    const xodCore = path.resolve(workspace, './lib/xod/core');
+    const xodCoreOwner = path.resolve(xodCore, '..');
+    Loader.loadProjectWithoutLibs(xodCore)
+          .then((project) => {
+            const implsLoaded = shell
+              .ls(`${xodCore}/*.{c,cpp,h,inl,js}`)
+              .every((implPath) => {
+                const { base, dir } = path.parse(implPath);
+                const impl = fs.readFileSync(implPath).toString();
+                const xodm = path.relative(xodCoreOwner, `${dir}/patch.xodm`);
+                const patch = project.find(_ => _.path === xodm);
+                if (!(patch && patch.impls)) return false;
+                return patch.impls[base] === impl;
+              });
+            done(
+              !implsLoaded && new Error('some implementations were not loaded')
+            );
+          })
+          .catch(done);
   });
 });
