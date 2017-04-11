ad20a33e963038148555c5e0115c4992bef63585
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2017-04-11 15:55:10 +0300

    refactor(xod-client): use dump of empty v2 project with fresh stdlib as initial state instead of converting some legacy nodeTypes from xod-core
    This is a temporary measure until we implement loading stdlib dynamically

--- packages/xod-client/src/core/state.js
+++ packages/xod-client/src/core/state.js
@@ -1,22 +1,12 @@
-import R from 'ramda';
-import { toV2 } from 'xod-project';
-import core from 'xod-core';
-
 import editorState from '../editor/state';
 import projectState from '../project/state';
 import projectBrowserState from '../projectBrowser/state';
 
-const initialState = {
-  project: projectState,
+export default {
+  projectV2: projectState,
   projectHistory: {},
   projectBrowser: projectBrowserState,
   editor: editorState,
   errors: {},
   processes: {},
 };
-
-export default R.assoc( // TODO: #migrateToV2
-  'projectV2',
-  toV2(core.getProjectPojo(initialState)),
-  initialState
-);
