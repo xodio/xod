b4b34f0928490687d1a7d2354e04712be37eadca
Alexander Matsievsky <am@amperka.ru>
2017-05-11 10:51:35 +0300

    feat(xod-client-electron): setup CI/CD
    Closes #495.

--- tools/travis.sh
+++ tools/travis.sh
@@ -1,6 +1,16 @@
+#!/usr/bin/env bash
+
 set -ev
 
 npm run verify
-if [ -z "$TRAVIS_TAG" ]; then
+
+tags=$(git tag --points-at "$TRAVIS_COMMIT")
+if [ -n "$tags" ]; then
     npm run electron-dist
+    config=$(mktemp)
+    echo "$GOOGLE_CLOUD_STORAGE_CONFIG" | base64 --decode >"$config"
+    while read -r tag; do
+        find packages/xod-client-electron/dist -maxdepth 1 -type f -exec node  \
+ tools/electron-upload.js --config="$config" --file={} --tag="$tag" \;
+    done <<<"$tags"
 fi
