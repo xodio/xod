70fa4fcb260eff6a3ea2577e39374c5f4673afbd
Victor Nakoryakov <nail.xx@gmail.com>
2017-05-12 17:53:01 +0300

    chore(infra): remove dead code

--- packages/xod-server/server/boot/root.js
+++
@@ -1,6 +0,0 @@
-module.exports = function rootBoot(server) {
-  // Install a `/` route that returns server status
-  const router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
-};
--- packages/xod-server/server/constants/errors.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable max-len */
-
-module.exports = {
-  project: {
-    validProjectName: 'Project name should contain only latin alphabet, digits, minus or underscore char.',
-  },
-};
