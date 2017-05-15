da40e5300c92d841c168595191829a43906cb74c
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2017-05-15 13:41:34 +0300

    refactor(xod-cli): remove doc command

--- packages/xod-cli/src/xodc.js
+++ packages/xod-cli/src/xodc.js
@@ -4,7 +4,6 @@ import { docopt } from 'docopt';
 import { runCommand } from './utils';
 
 import * as ab from './xodc-ab';
-import generateDoc from './xodc-doc';
 import install from './xodc-install';
 import pack from './xodc-pack';
 import publish from './xodc-publish';
@@ -19,9 +18,7 @@ XOD project: Command Line Interface
 Usage:
   xodc pack <projectDir> <output>
   xodc unpack <xodball> <workspace>
-  xodc migrate <input> <output>
   xodc transpile [--output=<filename>] [--target=<target>] <input> <path>
-  xodc doc [--clear] <outputDir> <templatesDir> <projectDir>
   xodc publish --author=<author> [--owner=<owner>] [<projectDir>]
   xodc install <library> [<path>]
   xodc ab set-executable <path>
@@ -37,9 +34,7 @@ Usage:
 Commands:
   pack                  Pack project directory into xodball.
   unpack                Unpack xodball into new project directory.
-  migrate               Migrate project into new version.
   transpile             Transpile project into device runtime.
-  doc                   Generate doc for project.
   publish               Publish a new library version.
   install               Install the library into workspace.
   ab set-executable     Set path to Arduino IDE executable.
@@ -65,10 +60,6 @@ const programs = {
     output: o['--output'],
     target: o['--target'],
   }),
-  doc: o => generateDoc(
-    o['<outputDir>'], o['<templatesDir>'], o['<projectDir>'],
-    { clear: o['--clear'] }
-  ),
   publish: o => publish(
     o['--author'], o['--owner'] || o['--author'], o['<projectDir>'] || '.'
   ),
