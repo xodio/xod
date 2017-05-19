f6a3249425d96f1a38f869eaebf8a0c082c00c15
Kirill Shumilov <brusher.designer@gmail.com>
2017-05-19 18:55:05 +0300

    test(xod-fs): rephrase test titles

--- packages/xod-fs/test/utils.spec.js
+++ packages/xod-fs/test/utils.spec.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import chai, { assert } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 
@@ -12,70 +13,79 @@ import { DEFAULT_WORKSPACE_PATH } from '../src/constants';
 chai.use(chaiAsPromised);
 
 describe('Utils', () => {
-  it('ensureWorkspacePath: resolve path and homedir symbol', () => {
+  it('ensureWorkspacePath: if path correct, just resolves path', () => {
     assert.equal(U.ensureWorkspacePath('/Users/vaso/test'), '/Users/vaso/test');
+    assert.equal(U.ensureWorkspacePath('a/b/c'), path.resolve('a/b/c'));
+  });
+  it('ensureWorkspacePath: if path begins with homedir symbol, resolves path to homedir', () => {
     assert.equal(U.ensureWorkspacePath('~/xod'), U.resolvePath('~/xod'));
   });
-  it('ensureWorkspacePath: fallback to default workspace path', () => {
+  it('ensureWorkspacePath: if path is empty or incorrect, fallbacks to default workspace path', () => {
     assert.equal(U.ensureWorkspacePath(''), U.resolvePath(DEFAULT_WORKSPACE_PATH));
     assert.equal(U.ensureWorkspacePath({}), U.resolvePath(DEFAULT_WORKSPACE_PATH));
     assert.equal(U.ensureWorkspacePath(null), U.resolvePath(DEFAULT_WORKSPACE_PATH));
   });
 
-  it('resolveWorkspacePath: resolve Path for valid value',
+  it('resolveWorkspacePath: if path correct, just resolves path',
     () => assert.eventually.equal(
       U.resolveWorkspacePath(fixture('./workspace')),
       fixture('./workspace')
     )
   );
-  it('resolveWorkspacePath: reject INVALID_WORKSPACE_PATH for null value',
-    () => expectRejectedWithCode(
-      U.resolveWorkspacePath(null),
-      ERROR_CODES.INVALID_WORKSPACE_PATH
-    )
-  );
-  it('resolveWorkspacePath: reject INVALID_WORKSPACE_PATH for empty string',
-    () => expectRejectedWithCode(
-      U.resolveWorkspacePath(''),
-      ERROR_CODES.INVALID_WORKSPACE_PATH
-    )
+  it('resolveWorkspacePath: if path is incorrect, rejects with error code INVALID_WORKSPACE_PATH',
+    () => Promise.all([
+      expectRejectedWithCode(
+        U.resolveWorkspacePath(null),
+        ERROR_CODES.INVALID_WORKSPACE_PATH
+      ),
+      expectRejectedWithCode(
+        U.resolveWorkspacePath(''),
+        ERROR_CODES.INVALID_WORKSPACE_PATH
+      ),
+    ])
   );
 
-  it('isWorkspaceValid: resolve Path for valid workspace',
+  it('isWorkspaceValid: if workspace in specified path is valid, resolves path',
     () => assert.eventually.equal(
       U.isWorkspaceValid(fixture('./workspace')),
       fixture('./workspace')
     )
   );
-  it('isWorkspaceValid: reject WORKSPACE_DIR_NOT_EMPTY for not empty directory',
+  it('isWorkspaceValid: if specified directory is not empty, rejects with error code WORKSPACE_DIR_NOT_EMPTY',
     () => expectRejectedWithCode(
       U.isWorkspaceValid(fixture('./emptyWorkspace')),
       ERROR_CODES.WORKSPACE_DIR_NOT_EMPTY
     )
   );
-  it('isWorkspaceValid: reject WORKSPACE_DIR_NOT_EXIST_OR_EMPTY for non-existent directory',
+  it('isWorkspaceValid: if specified directory is not exist or empty, rejects with error code WORKSPACE_DIR_NOT_EXIST_OR_EMPTY',
     () => expectRejectedWithCode(
       U.isWorkspaceValid(fixture('./notExistWorkspace')),
       ERROR_CODES.WORKSPACE_DIR_NOT_EXIST_OR_EMPTY
     )
   );
 
-  it('validateWorkspace: resolve Path for valid workspace',
+  it('validateWorkspace: if workspace in specified path is valid, resolves path',
     () => assert.eventually.equal(
       U.validateWorkspace(fixture('./workspace')),
       fixture('./workspace')
     )
   );
-  it('validateWorkspace: reject WORKSPACE_DIR_NOT_EXIST_OR_EMPTY for not existent directory',
+  it('validateWorkspace: if specified path is incorrect, rejects with error code INVALID_WORKSPACE_PATH',
     () => expectRejectedWithCode(
-      U.validateWorkspace(fixture('./notExistWorkspace')),
-      ERROR_CODES.WORKSPACE_DIR_NOT_EXIST_OR_EMPTY
+      U.validateWorkspace(null),
+      ERROR_CODES.INVALID_WORKSPACE_PATH
     )
   );
-  it('validateWorkspace: reject WORKSPACE_DIR_NOT_EMPTY for not empty directory without workspace file',
+  it('validateWorkspace: if specified directory is not empty, rejects with error code WORKSPACE_DIR_NOT_EMPTY',
     () => expectRejectedWithCode(
       U.validateWorkspace(fixture('.')),
       ERROR_CODES.WORKSPACE_DIR_NOT_EMPTY
+    )
+  );
+  it('validateWorkspace: if specified directory is not exist or empty, rejects with error code WORKSPACE_DIR_NOT_EXIST_OR_EMPTY',
+    () => expectRejectedWithCode(
+      U.validateWorkspace(fixture('./notExistWorkspace')),
+      ERROR_CODES.WORKSPACE_DIR_NOT_EXIST_OR_EMPTY
     )
   );
 });
