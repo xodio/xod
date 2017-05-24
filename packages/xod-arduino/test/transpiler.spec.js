a9fbcb664a009a84576806bbc854e6457d2a9ee7
Kirill Shumilov <brusher.designer@gmail.com>
2017-05-24 13:27:52 +0300

    test(xod-arduino): add test with “pot-led”. `pot-led.project.json` is an actual version of project that generated by IDE (with new bound values and etc)

--- packages/xod-arduino/test/transpiler.spec.js
+++ packages/xod-arduino/test/transpiler.spec.js
@@ -7,17 +7,27 @@ import transpile from '../src/transpiler';
 import blinkProject from './fixtures/blink.project.json';
 import blinkCpp from './fixtures/blink.cpp';
 
+import potLedProject from './fixtures/pot-led.project.json';
+import potLedCpp from './fixtures/pot-led.cpp';
+
 chai.use(chaiString);
 
-describe('xod-arduino transpiler (end-to-end test)', () => {
-  it('should return Either.Right with C++ code', () => {
+describe('xod-arduino transpiler (end-to-end tests)', () => {
+  it('Transpile Blink project with "@/main" entry-point patch should return Either.Right with C++ code', () => {
     foldEither(
       (err) => { throw err; },
       cpp => assert.equalIgnoreSpaces(cpp, blinkCpp),
       transpile(blinkProject, '@/main')
     );
   });
-  it('should return Either.Left with error if entry-point patch not found', () => {
+  it('Transpile PotLed project with "@/main" entry-point patch should return Either.Right with C++ code', () => {
+    foldEither(
+      (err) => { throw err; },
+      cpp => assert.equalIgnoreSpaces(cpp, potLedCpp),
+      transpile(potLedProject, '@/main')
+    );
+  });
+  it('Transpile Blink project with non-existing-patch as entry-point should return Either.Left with error if entry-point patch not found', () => {
     const r = transpile(blinkProject, '@/non-existing-patch');
     assert.equal(r.isLeft, true);
   });
