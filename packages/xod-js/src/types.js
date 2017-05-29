d7071e80e710bae8a76f637ea27fe87ec4d54508
Kirill Shumilov <brusher.designer@gmail.com>
2017-05-29 16:47:22 +0300

    tweak(xod-js): use pin labels instead of keys in the transpiled code, also refactored using of `xod/core/constant-*` instead of hardcoded magic nodes with value property

--- packages/xod-js/src/types.js
+++ packages/xod-js/src/types.js
@@ -1,5 +1,42 @@
 import HMDef from 'hm-def';
-import { env } from 'xod-project';
+import { env as xEnv, PinKey, PinLabel, DataType, DataValue } from 'xod-project';
+import XF from 'xod-func-tools';
+
+/* Types are by convention starts with a capital leter, so: */
+/* eslint-disable new-cap */
+
+const packageName = 'xod-arduino';
+const docUrl = 'http://xod.io/docs/dev/xod-arduino/#';
+
+//-----------------------------------------------------------------------------
+//
+// Type utilities
+//
+//-----------------------------------------------------------------------------
+
+const Model = XF.Model(packageName, docUrl);
+
+//-----------------------------------------------------------------------------
+//
+// Domain types
+//
+//-----------------------------------------------------------------------------
+
+export const TPin = Model('TPin', {
+  key: PinKey,
+  type: DataType,
+  label: PinLabel,
+  value: DataValue,
+});
+
+//-----------------------------------------------------------------------------
+//
+// Environment
+//
+//-----------------------------------------------------------------------------
+const env = xEnv.concat([
+  TPin,
+]);
 
 export const def = HMDef.create({
   checkTypes: process.env.NODE_ENV !== 'production',
--- packages/xod-js/test/transpiler.spec.js
+++ packages/xod-js/test/transpiler.spec.js
@@ -1,10 +1,11 @@
+import fs from 'fs';
 import R from 'ramda';
-import chai, { expect } from 'chai';
+import chai, { expect, assert } from 'chai';
 import dirtyChai from 'dirty-chai';
 
-import { createPatch } from 'xod-project';
+import { createPatch, listNodes, listLinks, getNodeId, getLinkId } from 'xod-project';
 import { defaultizeProject } from 'xod-project/test/helpers';
-import { foldEither } from 'xod-func-tools';
+import { foldEither, explode } from 'xod-func-tools';
 import * as Transpiler from '../src/transpiler';
 import easy from './fixtures/easy.txt';
 
@@ -119,17 +120,17 @@ describe('Transpiler', () => {
                 x: 0,
                 y: 0,
               },
-              label: '',
+              label: 'A',
               description: '',
             },
             in_B: {
               id: 'in_B',
-              type: 'xod/patch-nodes/input-string',
+              type: 'xod/patch-nodes/input-number',
               position: {
                 x: 200,
                 y: 0,
               },
-              label: '',
+              label: 'B',
               description: '',
             },
             out: {
@@ -143,10 +144,52 @@ describe('Transpiler', () => {
               description: '',
             },
           },
-          links: {},
+          links: {
+            'a-to-out': {
+              id: 'a-to-out',
+              input: {
+                nodeId: 'out',
+                pinKey: '__in__',
+              },
+              output: {
+                nodeId: 'in_A',
+                pinKey: '__out__',
+              },
+            },
+          },
           impls: {
             js: '// ok',
           },
+        },
+        'xod/core/constant-number': {
+          description: '',
+          nodes: {
+            noNativeImpl: {
+              label: '',
+              description: '',
+              boundValues: {},
+              id: 'noNativeImpl',
+              position: {
+                x: 100,
+                y: 100,
+              },
+              type: 'xod/patch-nodes/not-implemented-in-xod',
+            },
+            VAL: {
+              label: 'VAL',
+              description: '',
+              boundValues: {},
+              id: 'VAL',
+              type: 'xod/patch-nodes/output-number',
+              position: {
+                x: 0,
+                y: 300,
+              },
+            },
+          },
+          links: {},
+          impls: { js: '//const-number' },
+          path: 'xod/core/constant-number',
         },
       },
     });
@@ -154,21 +197,37 @@ describe('Transpiler', () => {
       project.patches['@/main'],
       {
         nodes: {
-          '0_in_A': {
-            id: '0_in_A',
-            type: 'xod/internal/const',
-            value: 10,
+          '0_A': {
+            id: '0_A',
+            type: 'xod/core/constant-number',
             position: { x: 0, y: 0 },
-            boundValues: {},
+            boundValues: {
+              VAL: 10,
+            },
+            label: '',
+            description: '',
+          },
+          '0_B': {
+            id: '0_B',
+            type: 'xod/core/constant-number',
+            position: { x: 0, y: 0 },
+            boundValues: {
+              VAL: 0,
+            },
             label: '',
             description: '',
           },
         },
         links: {
-          '0_in_A-to-0': {
-            id: '0_in_A-to-0',
+          '0_A-to-0': {
+            id: '0_A-to-0',
             input: { nodeId: '0', pinKey: 'in_A' },
-            output: { nodeId: '0_in_A', pinKey: 'value' },
+            output: { nodeId: '0_A', pinKey: 'VAL' },
+          },
+          '0_B-to-0': {
+            id: '0_B-to-0',
+            input: { nodeId: '0', pinKey: 'in_B' },
+            output: { nodeId: '0_B', pinKey: 'VAL' },
           },
         },
       }
@@ -178,15 +237,15 @@ describe('Transpiler', () => {
         id: '0',
         implId: 'xod/nodes/test',
         inputTypes: {
-          in_A: Number,
-          in_B: String,
+          A: Number,
+          B: Number,
         },
         outLinks: {
-          out: [{
-            key: 'in_A',
+          OUT: [{
+            key: 'A',
             nodeId: '1',
           }, {
-            key: 'in_B',
+            key: 'B',
             nodeId: '1',
           }],
         },
@@ -195,19 +254,31 @@ describe('Transpiler', () => {
         id: '1',
         implId: 'xod/nodes/test',
         inputTypes: {
-          in_A: Number,
-          in_B: String,
+          A: Number,
+          B: Number,
         },
         outLinks: {},
       },
       {
-        id: '0_in_A',
-        implId: 'xod/internal/const',
+        id: '0_A',
+        implId: 'xod/core/constant-number',
         value: 10,
         inputTypes: {},
         outLinks: {
-          value: [{
-            key: 'in_A',
+          VAL: [{
+            key: 'A',
+            nodeId: '0',
+          }],
+        },
+      },
+      {
+        id: '0_B',
+        implId: 'xod/core/constant-number',
+        value: 0,
+        inputTypes: {},
+        outLinks: {
+          VAL: [{
+            key: 'B',
             nodeId: '0',
           }],
         },
@@ -230,81 +301,75 @@ describe('Transpiler', () => {
         const patch = project.patches['xod/nodes/test'];
         expect(Transpiler.getInputTypes(patch))
           .to.be.deep.equal({
-            in_A: Number,
-            in_B: String,
+            A: Number,
+            B: Number,
           });
       });
     });
 
     describe('getOutLinks', () => {
       it('should return an empty object for empty patch', () => {
-        expect(Transpiler.getOutLinks('0', createPatch()))
+        expect(Transpiler.getOutLinks('0', project, createPatch()))
           .to.be.an('object')
           .and.empty();
       });
       it('should return an empty object for node without out links', () => {
         const patch = project.patches['@/main'];
-        expect(Transpiler.getOutLinks('1', patch))
+        expect(Transpiler.getOutLinks('1', project, patch))
           .to.be.an('object')
           .and.empty();
       });
       it('should return a correct output links', () => {
         const patch = project.patches['@/main'];
-        expect(Transpiler.getOutLinks('0', patch))
+        expect(Transpiler.getOutLinks('0', project, patch))
           .to.be.deep.equal({
-            out: [{
-              key: 'in_A',
+            OUT: [{
+              key: 'A',
               nodeId: '1',
             }, {
-              key: 'in_B',
+              key: 'B',
               nodeId: '1',
             }],
           });
       });
     });
 
-    describe('getBoundValuePairs', () => {
-      it('should return empty array for node without curried pins', () => {
-        const node = project.patches['@/main'].nodes['1'];
-        expect(Transpiler.getBoundValuePairs(node))
-          .to.be.an('array')
-          .and.empty();
-      });
-      it('should return array with one pair [pinKey, pinValue] for node with one curried pin', () => {
-        const node = project.patches['@/main'].nodes['0'];
-        expect(Transpiler.getBoundValuePairs(node))
-          .to.be.deep.equal([['in_A', 10]]);
-      });
-    });
-
     describe('addConstNodesToPatch', () => {
       it('should return the same patch', () => {
         const patch = project.patches['xod/nodes/test'];
-        expect(Transpiler.addConstNodesToPatch(project, patch))
+        expect(Transpiler.addConstNodesToPatch(project, project, patch))
           .to.be.deep.equal(patch);
       });
       it('should return patch with additional node and additional link', () => {
         const patch = project.patches['@/main'];
-        expect(Transpiler.addConstNodesToPatch(project, patch))
+        expect(Transpiler.addConstNodesToPatch(project, project, patch))
           .to.be.deep.equal(patchWithConst);
       });
     });
 
     describe('transformPatch', () => {
       it('should return Maybe.Nothing for non-existing path', () => {
-        expect(Transpiler.transformPatch('non/existing/path', project).isNothing).to.be.true();
+        expect(Transpiler.transformPatch('non/existing/path', project, project).isNothing).to.be.true();
       });
       it('should return Maybe.Just for existing path with transformed patch', () => {
-        const newPatch = Transpiler.transformPatch('@/main', project);
+        const newPatch = Transpiler.transformPatch('@/main', project, project);
         expect(newPatch.isJust).to.be.true();
 
-        const newNodes = R.keys(newPatch.chain(R.prop('nodes')));
-        const newLinks = R.keys(newPatch.chain(R.prop('links')));
+        const newNodes = R.compose(
+          R.map(getNodeId),
+          listNodes,
+          explode
+        )(newPatch);
+        const newLinks = R.compose(
+          R.map(getLinkId),
+          listLinks,
+          explode
+        )(newPatch);
 
         expect(newNodes)
-          .to.include.members(['0', '1', '0_in_A']);
+          .to.include.members(['0', '1', '0_A', '0_B']);
         expect(newLinks)
-          .to.include.members(['l1', 'l2', '0_in_A-to-0']);
+          .to.include.members(['l1', 'l2', '0_A-to-0', '0_B-to-0']);
       });
     });
 
@@ -315,8 +380,8 @@ describe('Transpiler', () => {
         expect(Transpiler.transformNode(patch, project, node))
           .to.be.deep.equal(expectedNodes[1]);
       });
-      it('should return the transformed xod/internal/const node', () => {
-        const node = patchWithConst.nodes['0_in_A'];
+      it('should return the transformed xod/core/constant-number node', () => {
+        const node = patchWithConst.nodes['0_A'];
         expect(Transpiler.transformNode(patchWithConst, project, node))
           .to.be.deep.equal(expectedNodes[2]);
       });
@@ -432,11 +497,42 @@ describe('Transpiler', () => {
             },
             links: {},
             impls: {
-              js: '// ok',
+              js: '/*xod/nodes/test*/',
             },
+          },
+          'xod/core/constant-number': {
+            description: '',
+            nodes: {
+              noNativeImpl: {
+                label: '',
+                description: '',
+                boundValues: {},
+                id: 'noNativeImpl',
+                position: {
+                  x: 100,
+                  y: 100,
+                },
+                type: 'xod/patch-nodes/not-implemented-in-xod',
+              },
+              VAL: {
+                label: 'VAL',
+                description: '',
+                boundValues: {},
+                id: 'VAL',
+                type: 'xod/patch-nodes/output-number',
+                position: {
+                  x: 0,
+                  y: 300,
+                },
+              },
+            },
+            links: {},
+            impls: { js: '/*xod/core/constant-number*/' },
+            path: 'xod/core/constant-number',
           },
         },
       });
+
       const eitherResult = Transpiler.default({
         project,
         path: '@/main',
@@ -449,7 +545,7 @@ describe('Transpiler', () => {
       // of raw-loader to run tests for runtime.spec.js
       foldEither(
         err => expect.fail(err.message),
-        result => expect(result.trim()).to.be.equal(easy.trim()),
+        result => assert.strictEqual(result.trim(), easy.trim(), 'JS transpiled code is not equal to expected JS code'),
         eitherResult
       );
     });
