09a6c240f103a30449e03f5ba22676301fbfe9fb
Kirill Shumilov <brusher.designer@gmail.com>
2017-05-30 20:23:29 +0300

    test(xod-cli): add test for transpiling Blink project for arduino using xodc

---
+++ packages/xod-cli/test/transpile.spec.js
@@ -0,0 +1,27 @@
+import fs from 'fs-extra';
+import path from 'path';
+import { assert } from 'chai';
+import { exec } from 'child-process-promise';
+
+import { rmrf } from 'xod-fs';
+
+const tmpPath = subpath => path.resolve(__dirname, './tmp', subpath);
+const xodc = path.resolve(__dirname, '../bin/xodc');
+const wsPath = subpath => path.resolve(__dirname, '../../../workspace', subpath);
+
+describe('xodc transpile', () => {
+  afterEach(() => rmrf(tmpPath('./')));
+
+  it('should transpile Blink project for Arduino', () =>
+    exec(`node ${xodc} transpile --target=arduino --output=${tmpPath('blink.cpp')} ${wsPath('blink')} @/main`)
+      .then(() => Promise.all([
+        fs.readFile(tmpPath('blink.cpp'), 'utf-8'),
+        fs.readFile(wsPath('blink/__fixtures__/arduino.cpp'), 'utf-8'),
+      ]))
+      .then(([actual, expected]) => assert.strictEqual(
+        actual,
+        expected,
+        'expected and actual C++ don’t match'
+      ))
+  );
+});
