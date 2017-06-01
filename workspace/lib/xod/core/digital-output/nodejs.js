ae4fdb2d7804c1eed396a85b551ff3f7557da883
Kirill Shumilov <brusher.designer@gmail.com>
2017-06-01 21:56:19 +0300

    refactor(xod-js): update runtime and transpiler to work properly with bounded output values (and default ones)

--- workspace/lib/xod/core/digital-output/nodejs.js
+++ workspace/lib/xod/core/digital-output/nodejs.js
@@ -17,31 +17,28 @@ function getRaspberryPortDirection(port) {
   return getRaspberryPort(port) + 'direction';
 }
 
-function writeFile(filename, data, callback) {
-  return new Promise(function(resolve, reject) {
-    fs.writeFile(filename, data, function(err) {
-      if (err) return reject(err);
-      callback(resolve, reject);
-    });
-  });
-}
-
-function exportPort(port) {
-  return writeFile(
+function exportPort(port, cb) {
+  fs.writeFile(
     '/sys/class/gpio/export',
     port,
-    function(resolve) { resolve(port); }
+    function writePort(err) {
+      if (err) throw err;
+      cb(port);
+    }
   );
 }
 
-function setPortDirection(direction, port) {
+function setPortDirection(direction, port, cb) {
   if (direction !== 'in' && direction !== 'out') {
     throw new Error('Wrong direction `' + direction + '` for Pin `' + port + '`');
   }
-  return writeFile(
+  fs.writeFile(
     getRaspberryPortDirection(port),
     direction,
-    function(resolve) { resolve(port); }
+    function writeDirection(err) {
+      if (err) throw err;
+      cb(port);
+    }
   );
 }
 
@@ -49,16 +46,19 @@ function setPortDirection(direction, port) {
  * To work with Raspberry ports we have to export it
  * and then set its direction. To prevent blocking of
  * the eventloop of NodeJS we use the asynchronous
- * version of fs.writeFile wrapped by Promises.
+ * version of fs.writeFile.
  *
  * In case that we have already exported this port:
  * just skip it and return resolved port number.
  * Otherwise: export and set direction for the new port.
  */
-function preparePort(direction, oldPort, newPort) {
-  if (oldPort === newPort) { return Promise.resolve(newPort); }
-  return exportPort(newPort).then(function(port) {
-    return setPortDirection(direction, port);
+function preparePort(direction, oldPort, newPort, cb) {
+  if (oldPort === newPort) return cb(newPort);
+
+  exportPort(newPort, function exportPortCallback(port) {
+    setPortDirection(direction, port, function setDirectionCallback() {
+      cb(port);
+    });
   });
 }
 
@@ -74,14 +74,11 @@ module.exports.setup = function(e) {
 module.exports.evaluate = function(e) {
   var port = e.context.port;
 
-  preparePort('out', port, e.inputs.PORT)
-    .then(function(_port) {
-      e.context.port = _port;
-      return writeFile(
-        getRaspberryPortValue(_port),
-        +e.inputs.SIG,
-        function(resolve) { resolve(_port); }
-      )
-    })
-    .catch(function(err) { console.error(err); });
+  preparePort('out', port, e.inputs.PORT, function evaluateDigitalOutput(_port) {
+    e.context.port = _port;
+    fs.writeFile(
+      getRaspberryPortValue(_port),
+      +e.inputs.SIG
+    );
+  });
 };
