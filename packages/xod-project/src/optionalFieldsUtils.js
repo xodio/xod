14cd8c6d533a0e58f1ca8546c775fd60f2c04ae0
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2017-07-18 16:43:56 +0300

    chore(xod-project): cover optionalFieldsUtils with hm-def

--- packages/xod-project/src/optionalFieldsUtils.js
+++ packages/xod-project/src/optionalFieldsUtils.js
@@ -1,6 +1,8 @@
 import R from 'ramda';
 import { subtractObject } from 'xod-func-tools';
 
+import { def } from './types';
+
 export const OPTIONAL_NODE_FIELDS = {
   boundValues: {},
   description: '',
@@ -23,10 +25,8 @@ export const OPTIONAL_PROJECT_FIELDS = {
   description: '',
 };
 
-// :: ??? -> Node
 export const addMissingOptionalNodeFields = R.merge(OPTIONAL_NODE_FIELDS);
 
-// :: ??? -> Patch
 export const addMissingOptionalPatchFields = R.compose(
   R.evolve({
     nodes: R.map(addMissingOptionalNodeFields),
@@ -34,7 +34,6 @@ export const addMissingOptionalPatchFields = R.compose(
   R.merge(OPTIONAL_PATCH_FIELDS)
 );
 
-// :: ??? -> Project
 export const addMissingOptionalProjectFields = R.compose(
   R.evolve({
     patches: R.map(addMissingOptionalPatchFields),
@@ -42,21 +41,27 @@ export const addMissingOptionalProjectFields = R.compose(
   R.merge(OPTIONAL_PROJECT_FIELDS)
 );
 
-// Node -> ???
-export const omitEmptyOptionalNodeFields = subtractObject(OPTIONAL_NODE_FIELDS);
-
-// :: Patch -> ???
-export const omitEmptyOptionalPatchFields = R.compose(
-  R.evolve({
-    nodes: R.map(omitEmptyOptionalNodeFields),
-  }),
-  subtractObject(OPTIONAL_PATCH_FIELDS)
+export const omitEmptyOptionalNodeFields = def(
+  'omitEmptyOptionalNodeFields :: Node -> Object',
+  subtractObject(OPTIONAL_NODE_FIELDS)
 );
 
-// :: Project -> ???
-export const omitEmptyOptionalProjectFields = R.compose(
-  R.evolve({
-    patches: R.map(omitEmptyOptionalPatchFields),
-  }),
-  subtractObject(OPTIONAL_PROJECT_FIELDS)
+export const omitEmptyOptionalPatchFields = def(
+  'omitEmptyOptionalPatchFields :: Patch -> Object',
+  R.compose(
+    R.evolve({
+      nodes: R.map(omitEmptyOptionalNodeFields),
+    }),
+    subtractObject(OPTIONAL_PATCH_FIELDS)
+  )
+);
+
+export const omitEmptyOptionalProjectFields = def(
+  'omitEmptyOptionalProjectFields :: Project -> Object',
+  R.compose(
+    R.evolve({
+      patches: R.map(omitEmptyOptionalPatchFields),
+    }),
+    subtractObject(OPTIONAL_PROJECT_FIELDS)
+  )
 );
