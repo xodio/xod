fd919d1daed3ba0cf58078c1390021c2c94da500
Kirill Shumilov <brusher.designer@gmail.com>
2017-08-03 20:55:28 +0300

    refactor(xod-cli, xod-arduino-builder): get rid of `xod-arduino-builder` package and clean it up from `xod-cli`

--- packages/xod-cli/src/xodc.js
+++ packages/xod-cli/src/xodc.js
@@ -4,7 +4,6 @@ import { docopt } from 'docopt';
 import sourceMapSupport from 'source-map-support';
 import { runCommand } from './utils';
 
-import * as ab from './xodc-ab';
 import install from './xodc-install';
 import pack from './xodc-pack';
 import publish from './xodc-publish';
@@ -30,15 +29,6 @@ Usage:
   xodc transpile [--output=<filename>] [--target=<target>] <input> <path>
   xodc publish [--swagger=<swagger>] [--orgname=<orgname>] [<projectDir>]
   xodc install --swagger=<swagger> <libUri> [<path>]
-  xodc ab set-executable <path>
-  xodc ab set-packages <path>
-  xodc ab list-index
-  xodc ab list-pavs
-  xodc ab list-ports
-  xodc ab list-boards <package> <architecture> <version>
-  xodc ab install-pav <package> <architecture> <version>
-  xodc ab compile <package> <architecture> <board> <file>
-  xodc ab upload <package> <architecture> <board> <port> <file>
 
 Commands:
   pack                  Pack project directory into xodball.
@@ -46,15 +36,6 @@ Commands:
   transpile             Transpile project into device runtime.
   publish               Publish a new library version.
   install               Install the library into workspace.
-  ab set-executable     Set path to Arduino IDE executable.
-  ab set-packages       Set path to Arduino IDE packages.
-  ab list-index         List the raw official Arduino package index.
-  ab list-pav           List the processed official Arduino package index.
-  ab list-port          List the available ports.
-  ab list-boards        List the boards supported by the PAV.
-  ab install-pav        Install the PAV.
-  ab compile            Compile the file for the PAB.
-  ab upload             Upload the file for the PAB to port.
 
 Options:
   --target=<target>     Target device for transpilation [default: espruino].
@@ -77,26 +58,6 @@ const programs = {
     o['--swagger'] || PM_SWAGGER_URL,
     o['<libUri>'],
     o['<path>'] || '.'),
-  ab: o => runCommand(o, {
-    'set-executable': () => ab.setExecutable(o['<path>']),
-    'set-packages': () => ab.setPackages(o['<path>']),
-    'list-index': () => ab.listIndex(),
-    'list-pavs': () => ab.listPavs(),
-    'list-ports': () => ab.listPorts(),
-    'list-boards': () => ab.listBoards(
-      o['<package>'], o['<architecture>'], o['<version>']
-    ),
-    'install-pav': () => ab.installPav(
-      o['<package>'], o['<architecture>'], o['<version>']
-    ),
-    compile: () => ab.compile(
-      o['<package>'], o['<architecture>'], o['<board>'], o['<file>']
-    ),
-    upload: () => ab.upload(
-      o['<package>'], o['<architecture>'], o['<board>'], o['<port>'],
-      o['<file>']
-    ),
-  }),
 };
 
 // Running command
