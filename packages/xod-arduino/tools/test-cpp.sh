364046e146bfb677286ae7a0ef19b8705ab42ac8
Victor Nakoryakov <nail.xx@gmail.com>
2017-08-24 16:40:25 +0300

    test(xod-arduino): add test on compiled firmware size

--- packages/xod-arduino/tools/test-cpp.sh
+++ packages/xod-arduino/tools/test-cpp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 DIR=test-cpp
 RUNNER=$DIR/run-tests
 
-g++ -g -O0 -o $RUNNER $DIR/test.cpp $DIR/list.cpp
+g++ -std=c++11 -g -O0 -o $RUNNER $DIR/test.cpp $DIR/list.cpp
 
 if [[ $* == *--leak-check* ]]; then
     valgrind --leak-check=yes $RUNNER
