4f8e9ba03d1830a8f8f4c5719e15494ada6f39d1
Victor Nakoryakov <nail.xx@gmail.com>
2017-10-26 17:06:24 +0300

    chore(tools): implement a script to update binary (hex/bin) fixtures

---
+++ tools/update-compiled-fixtures.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+function update_fixture {
+  echo ""
+  echo "Building $3 from $2 for board $1..."
+  echo ""
+
+  BDIR=$(mktemp -d /tmp/xod-fixture-XXXXXX)
+  platformio ci \
+    --board $1 \
+    --build-dir $BDIR \
+    --keep-build-dir \
+    ${2}/__fixtures__/arduino.cpp
+
+  cp $BDIR/.pioenvs/${1}/${3} ${2}/__fixtures__
+}
+
+update_fixture uno ./workspace/blink firmware.hex
+update_fixture due ./workspace/blink firmware.bin
