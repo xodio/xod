4fa50a5f29ded00488a6e7c45361f7f39b481d0c
Kirill Shumilov <brusher.designer@gmail.com>
2017-12-22 17:31:51 +0300

    tweak(xod-client): update styles of Breadcrumbs and CodeEditor

--- packages/xod-client/src/editor/components/CppImplementationEditor.jsx
+++ packages/xod-client/src/editor/components/CppImplementationEditor.jsx
@@ -4,12 +4,19 @@ import cn from 'classnames';
 import CodeMirror from 'react-codemirror';
 import 'codemirror/mode/clike/clike';
 
-const CppImplementationEditor = ({ isActive, source, isInDebuggerTab, onChange, onClose }) => {
+const CppImplementationEditor = ({
+  patchPath,
+  isActive,
+  source,
+  isInDebuggerTab,
+  onChange,
+  onClose,
+}) => {
   const options = {
     lineNumbers: true,
     readOnly: isInDebuggerTab,
     mode: 'text/x-c++src',
-    theme: 'material',
+    theme: 'xod',
     indentUnit: 4,
   };
 
@@ -20,12 +27,31 @@ const CppImplementationEditor = ({ isActive, source, isInDebuggerTab, onChange, 
         isInDebuggerTab,
       })}
     >
-      <div className="toolbar">
-        <button className="back-button" onClick={onClose}>
-          <i className="fa fa-arrow-left" />&nbsp;Back
-        </button>
-        <span className="title">{isInDebuggerTab ? 'Viewing' : 'Editing'} node C++ implementation</span>
-      </div>
+      <ul className="Breadcrumbs Breadcrumbs--codeEditor">
+        <li>
+          <button className="back-button" onClick={onClose} />
+        </li>
+        <li>
+          <button
+            className="Breadcrumbs-chunk-button"
+            onClick={onClose}
+          >
+            {patchPath}
+          </button>
+        </li>
+        <li>
+          <button
+            className="Breadcrumbs-chunk-button is-tail is-active"
+          >
+            C++ implementation
+            {isInDebuggerTab && (
+              <span className="hint">
+                read only
+              </span>
+            )}
+          </button>
+        </li>
+      </ul>
       <div className="editor">
         <CodeMirror
           value={source}
@@ -38,6 +64,7 @@ const CppImplementationEditor = ({ isActive, source, isInDebuggerTab, onChange, 
 };
 
 CppImplementationEditor.propTypes = {
+  patchPath: PropTypes.string.isRequired,
   isActive: PropTypes.bool.isRequired,
   source: PropTypes.string.isRequired,
   isInDebuggerTab: PropTypes.bool.isRequired,
