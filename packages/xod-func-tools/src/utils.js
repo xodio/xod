201f8f7d5ff540458a5ac952e7164390aca14f94
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2018-04-18 11:45:00 +0300

    feat(xod-func-tools): add enumerate util to produce human-readable enumerations

--- packages/xod-func-tools/src/utils.js
+++ packages/xod-func-tools/src/utils.js
@@ -10,3 +10,15 @@ export const notNil = R.complement(R.isNil);
 export const notEmpty = R.complement(R.isEmpty);
 export const hasNo = R.complement(R.has);
 export const notEquals = R.complement(R.equals);
+
+// enumerate(', ', ' or ', [1,2,3,4]) => "1, 2, 3 or 4"
+export const enumerate = R.curry((separator, lastSeparator, items) => {
+  const firstItems = R.dropLast(2, items);
+  const lastItems = R.takeLast(2, items);
+
+  return R.compose(
+    R.join(separator),
+    R.append(R.__, firstItems),
+    R.join(lastSeparator)
+  )(lastItems);
+});
