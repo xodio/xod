28de4622abedc90f86581d065c34f15487472c5e
Kirill Shumilov <brusher.designer@gmail.com>
2018-05-24 14:04:28 +0300

    fix(xod-arduino): define Number always as a 4-byte float platform-independent

--- packages/xod-arduino/platform/runtime.cpp
+++ packages/xod-arduino/platform/runtime.cpp
@@ -106,7 +106,11 @@ namespace xod {
 //----------------------------------------------------------------------------
 // Type definitions
 //----------------------------------------------------------------------------
+#if __SIZEOF_FLOAT__ == 4
+typedef float Number;
+#else
 typedef double Number;
+#endif
 typedef bool Logic;
 typedef unsigned long TimeMs;
 typedef uint8_t DirtyFlags;
