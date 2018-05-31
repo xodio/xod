2764ca8aad4ae4713bc44b13695e5f8ffcd60372
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2018-05-31 13:01:11 +0300

    refactor(stdlib): make xod/bits use byte type
    Closes #1188

--- workspace/__lib__/xod/bits/bitwise-xor/patch.cpp
+++ workspace/__lib__/xod/bits/bitwise-xor/patch.cpp
@@ -7,7 +7,7 @@ struct State {
 {{ GENERATED_CODE }}
 
 void evaluate(Context ctx) {
-    uint8_t in1 = (uint8_t)getValue<input_IN1>(ctx);
-    uint8_t in2 = (uint8_t)getValue<input_IN2>(ctx);
+    auto in1 = getValue<input_IN1>(ctx);
+    auto in2 = getValue<input_IN2>(ctx);
     emitValue<output_OUT>(ctx, in1 ^ in2);
 }
