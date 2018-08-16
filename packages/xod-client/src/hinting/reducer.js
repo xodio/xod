dd77a43c082b138f7eb74a11db6aafbae10cc694
Kirill Shumilov <brusher.designer@gmail.com>
2018-08-16 12:31:25 +0300

    fix(xod-client): fix updating of hinting branches and cover with test

--- packages/xod-client/src/hinting/reducer.js
+++ packages/xod-client/src/hinting/reducer.js
@@ -6,36 +6,47 @@ import UPDATE_HINTING from './actionType';
 import { mergeErrors } from './validation';
 
 const errorsLens = R.lensProp('errors');
-
+// =============================================================================
 const getDeducedTypesFromAction = R.path(['payload', 'deducedTypes']);
 const getErrorsFromAction = R.path(['payload', 'errors']);
 const getPatchSearchDataFromAction = R.path(['payload', 'patchSearchData']);
+// =============================================================================
+const updateDeducedTypes = R.curry((action, state) =>
+  R.ifElse(
+    R.pipe(getDeducedTypesFromAction, notNil),
+    R.pipe(getDeducedTypesFromAction, R.assoc('deducedTypes', R.__, state)),
+    R.always(state)
+  )(action)
+);
+const updateErrors = R.curry((action, state) =>
+  R.ifElse(
+    R.pipe(getErrorsFromAction, notNil),
+    R.pipe(getErrorsFromAction, errs =>
+      R.over(errorsLens, mergeErrors(R.__, errs), state)
+    ),
+    R.always(state)
+  )(action)
+);
+const updatePatchSearchData = R.curry((action, state) =>
+  R.ifElse(
+    R.pipe(getPatchSearchDataFromAction, notNil),
+    R.pipe(
+      getPatchSearchDataFromAction,
+      R.assoc('patchSearchData', R.__, state)
+    ),
+    R.always(state)
+  )(action)
+);
+// =============================================================================
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_HINTING:
       return R.compose(
-        R.when(
-          R.pipe(getDeducedTypesFromAction, notNil),
-          R.pipe(
-            getDeducedTypesFromAction,
-            R.assoc('deducedTypes', R.__, state)
-          )
-        ),
-        R.when(
-          R.pipe(getErrorsFromAction, notNil),
-          R.pipe(getErrorsFromAction, errs =>
-            R.over(errorsLens, mergeErrors(R.__, errs), state)
-          )
-        ),
-        R.when(
-          R.pipe(getPatchSearchDataFromAction, notNil),
-          R.pipe(
-            getPatchSearchDataFromAction,
-            R.assoc('patchSearchData', R.__, state)
-          )
-        )
-      )(action);
+        updateDeducedTypes(action),
+        updateErrors(action),
+        updatePatchSearchData(action)
+      )(state);
     default:
       return state;
   }
