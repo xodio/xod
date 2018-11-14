11f80670a4647d3f59961e502c17e06b62de211d
Kirill Shumilov <brusher.designer@gmail.com>
2018-11-14 14:16:59 +0300

    feat(xod-tabtest): add tabtests and fix node `pad-with-zeroes` to work properly with NaN, Inf, OVF values

--- workspace/__lib__/xod/core/pad-with-zeroes/patch.cpp
+++ workspace/__lib__/xod/core/pad-with-zeroes/patch.cpp
@@ -12,11 +12,18 @@ struct State {
 void evaluate(Context ctx) {
     auto state = getState(ctx);
 
-    auto num = (getValue<input_IN>(ctx) == 0) ? 0 : abs(getValue<input_IN>(ctx));
-    int8_t lenFull = (getValue<input_W>(ctx) < 0) ? 0 : min((Number)15, getValue<input_W>(ctx));
+    auto val = getValue<input_IN>(ctx);
+    auto num = (val == 0) ? 0 : abs(val);
     char strNum[16];
-    dtostrf(num, 0, 0, strNum);
+    formatNumber(num, 0, strNum);
 
+    // If input value is NaN, Inf or OVF -> return without padding
+    if (isnan(val) || isinf(val) || num > 0x7FFFFFFF) {
+      strcpy(state->str, strNum);
+      return;
+    }
+
+    int8_t lenFull = (getValue<input_W>(ctx) < 0) ? 0 : min((Number)15, getValue<input_W>(ctx));
     int8_t lenStr = strlen(strNum);
     size_t zeroCount =  max(0, lenFull - lenStr);
     memset(state->str, '0', zeroCount);
