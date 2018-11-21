f3f752e88b165d4c562d8f3f210fdbee460bd743
Kirill Shumilov <brusher.designer@gmail.com>
2018-11-21 13:38:09 +0300

    fix(infra): update arduino-cli from 0.3.1 to 0.3.2

--- tools/install-arduinocli.ps1
+++ tools/install-arduinocli.ps1
@@ -1,8 +1,8 @@
 # Install arduino-cli
 
-Invoke-RestMethod -Uri "https://downloads.arduino.cc/arduino-cli/arduino-cli-0.3.1-alpha.preview-windows.zip" -Method GET -OutFile "$env:HOME/arduino-cli.zip"
+Invoke-RestMethod -Uri "https://downloads.arduino.cc/arduino-cli/arduino-cli-0.3.2-alpha.preview-windows.zip" -Method GET -OutFile "$env:HOME/arduino-cli.zip"
 unzip "$env:HOME/arduino-cli.zip" -d "$env:HOME"
-mv "$env:HOME/arduino-cli-0.3.1-alpha.preview-windows.exe" "$env:HOME/arduino-cli.exe"
+mv "$env:HOME/arduino-cli-0.3.2-alpha.preview-windows.exe" "$env:HOME/arduino-cli.exe"
 copy "$env:HOME/arduino-cli.exe" "./packages/xod-client-electron/arduino-cli.exe"
 
 $env:XOD_ARDUINO_CLI="$env:HOME/arduino-cli.exe"
