266619b1d3fe2260cbf0c36fe5b9ff305792d480
Kirill Shumilov <brusher.designer@gmail.com>
2018-12-28 17:05:33 +0300

    fix(xod-client-electron): make "update available" message persistent

--- packages/xod-client-electron/src/shared/composeMessage.js
+++ packages/xod-client-electron/src/shared/composeMessage.js
@@ -1,8 +1,9 @@
 // Duplicate of `composeMessage` from `xod-client`
 // It's necessary to prevent errors with importing `xod-client`,
 // that contains styles and react components.
-export default (title, note = null, button = null) => ({
+export default (title, note = null, button = null, persistent = false) => ({
   title,
   note,
   button,
+  persistent,
 });
