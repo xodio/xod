cefa3aa5f087167fe452ca6cad348902d6ed3317
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2019-06-20 21:13:36 +0300

    feat(stdlib): add pulse specialization for xod/core/error

---
+++ workspace/__lib__/xod/core/error(pulse)/patch.cpp
@@ -0,0 +1,15 @@
+#pragma XOD error_raise enable
+
+struct State {
+};
+
+{{ GENERATED_CODE }}
+
+void evaluate(Context ctx) {
+    if (getValue<input_ERR>(ctx)) {
+        raiseError(ctx);
+    } else {
+        if (isInputDirty<input_IN>(ctx))
+            emitValue<output_OUT>(ctx, 1);
+    }
+}
