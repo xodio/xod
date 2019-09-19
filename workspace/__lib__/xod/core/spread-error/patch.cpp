d941192931d8d425520baf8798e31cbbfdf7f90a
Kirill Shumilov <brusher.designer@gmail.com>
2019-09-19 14:49:33 +0300

    feat(stdlib): create `spread-error` node in `xod/core` library

---
+++ workspace/__lib__/xod/core/spread-error/patch.cpp
@@ -0,0 +1,16 @@
+#pragma XOD error_raise enable
+
+struct State {
+};
+
+{{ GENERATED_CODE }}
+
+void evaluate(Context ctx) {
+    if (getError<input_A>(ctx) || getError<input_B>(ctx)) {
+        raiseError(ctx);
+        return;
+    }
+
+    emitValue<output_AU0027>(ctx, getValue<input_A>(ctx));
+    emitValue<output_BU0027>(ctx, getValue<input_B>(ctx));
+}
