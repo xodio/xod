d69ddd89320bdf5d79c9737315163ea73a34b20f
Kirill Shumilov <brusher.designer@gmail.com>
2019-12-13 15:56:27 +0300

    fixup! feat(xod-client): support editing of the color literal in IDE

--- packages/xod-client/src/utils/normalizeColor.js
+++ packages/xod-client/src/utils/normalizeColor.js
@@ -2,10 +2,14 @@ import * as R from 'ramda';
 import { DEFAULT_VALUE_OF_TYPE, CUSTOM_TYPE } from 'xod-project';
 
 export default R.ifElse(
-  R.test(/^#([0-9a-f]{3}){1,2}$/i),
+  R.test(/^#?([0-9a-f]{3}){1,2}$/i),
   R.compose(
     R.when(str => str.length === 4, R.replace(/[^#]/g, '$&$&')),
-    R.toUpper
+    R.toUpper,
+    R.unless(
+      R.startsWith('#'),
+      R.concat('#')
+    )
   ),
   R.always(DEFAULT_VALUE_OF_TYPE[CUSTOM_TYPE.COLOR])
 );
--- packages/xod-client/test/normalizeColor.spec.js
+++ packages/xod-client/test/normalizeColor.spec.js
@@ -9,13 +9,16 @@ describe('normalizeColorLiteral', () => {
         expected,
         `${input} should be a ${expected}`
       );
+    expectColor('000', '#000000');
+    expectColor('fa9', '#FFAA99');
+    expectColor('ffaa76', '#FFAA76');
     expectColor('#000', '#000000');
     expectColor('#fa9', '#FFAA99');
     expectColor('#DEC', '#DDEECC');
     expectColor('#fa9de8', '#FA9DE8');
     expectColor('#0CDE87', '#0CDE87');
     // Fallback to default
-    expectColor('abc', '#000000');
+    expectColor('hello world', '#000000');
     expectColor('#gaf', '#000000');
   });
 });
