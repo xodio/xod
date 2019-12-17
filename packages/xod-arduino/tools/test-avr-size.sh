996f1987a6f97b2b87a77d7c47135ab86c73e084
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2019-12-17 17:00:14 +0300

    feat(infra): test multiple patches in `test-avr-size.sh`

--- packages/xod-arduino/tools/test-avr-size.sh
+++ packages/xod-arduino/tools/test-avr-size.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Testing size of fixtures compiled for AVR platform..."
 
-SELF_DIR=`dirname $0`
+SELF_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 WS_DIR="$SELF_DIR/../../../workspace"
 BUILD_DIR="$SELF_DIR/../.pio-build"
@@ -12,40 +12,50 @@ BUILD_DIR="$SELF_DIR/../.pio-build"
 AVR_SIZE=~/.platformio/packages/toolchain-atmelavr/bin/avr-size
 
 mkdir -p $BUILD_DIR
-platformio ci \
-  --board=uno \
-  --build-dir=$BUILD_DIR \
-  --keep-build-dir \
-  $WS_DIR/blink/__fixtures__/arduino.cpp \
-  > /dev/null
 
-AVR_SIZE_OUTPUT=$($AVR_SIZE -d -C --mcu=atmega328p $BUILD_DIR/.pioenvs/uno/firmware.elf)
-EXPECTED_OUTPUT="\
+test_size () {
+  echo "$1..."
+
+  platformio ci \
+    --board=uno \
+    --build-dir=$BUILD_DIR \
+    --keep-build-dir \
+    --project-option="lib_extra_dirs = $WS_DIR/__ardulib__" \
+    --verbose \
+    $WS_DIR/$1/__fixtures__/arduino.cpp \
+    > /dev/null
+
+  AVR_SIZE_OUTPUT=$($AVR_SIZE -d -C --mcu=atmega328p $BUILD_DIR/.pioenvs/uno/firmware.elf)
+  EXPECTED_OUTPUT="\
 AVR Memory Usage
 ----------------
 Device: atmega328p
 
-Program:    1582 bytes (4.8% Full)
+Program:$2
 (.text + .data + .bootloader)
 
-Data:         32 bytes (1.6% Full)
+Data:$3
 (.data + .bss + .noinit)"
 
-if [[ $AVR_SIZE_OUTPUT =~ "$EXPECTED_OUTPUT" ]]; then
-  echo "+ OK"
-else
-  echo "- FAIL"
-  echo
-  echo "Expected ====================================================="
-  echo
-  echo "$EXPECTED_OUTPUT"
-  echo
-  echo "Actual ======================================================="
-  echo
-  echo "$AVR_SIZE_OUTPUT"
-  echo
-  echo "If the size became better, fix expectation in"
-  echo "packages/xod-arduino/tools/test-avr-size.sh script,"
-  echo "and if the size became worse, fix C++"
-  exit 1
-fi
+  if [[ $AVR_SIZE_OUTPUT =~ "$EXPECTED_OUTPUT" ]]; then
+    echo "+ OK"
+  else
+    echo "- FAIL"
+    echo
+    echo "Expected ====================================================="
+    echo
+    echo "$EXPECTED_OUTPUT"
+    echo
+    echo "Actual ======================================================="
+    echo
+    echo "$AVR_SIZE_OUTPUT"
+    echo
+    echo "If the size became better, fix expectation in"
+    echo "packages/xod-arduino/tools/test-avr-size.sh script,"
+    echo "and if the size became worse, fix C++"
+    exit 1
+  fi
+}
+
+test_size "blink" "    1582 bytes (4.8% Full)" "         32 bytes (1.6% Full)"
+test_size "big-patch" "   27306 bytes (83.3% Full)" "       1016 bytes (49.6% Full)"
