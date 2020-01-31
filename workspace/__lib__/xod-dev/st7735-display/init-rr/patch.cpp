344f5209f2dd9c91799420e1ab4a22dd725e4db2
gabbapeople <gabbapeople@mail.ru>
2020-01-31 14:53:18 +0300

    feat(stdlib) : ST7735 based TFT LCD displays

---
+++ workspace/__lib__/xod-dev/st7735-display/init-rr/patch.cpp
@@ -0,0 +1,14 @@
+
+struct State {};
+
+// clang-format off
+{{ GENERATED_CODE }}
+// clang-format on
+
+void evaluate(Context ctx) {
+    if (!isSettingUp())
+        return;
+    auto dev = getValue<input_DEV>(ctx);
+    dev->initTypeRR();
+    emitValue<output_DEVU0027>(ctx, dev);
+}
---
+++ workspace/__lib__/xod-dev/st7735-display/st7735/patch.cpp
@@ -0,0 +1,40 @@
+
+// clang-format off
+{{#global}}
+#include <XST7735.h>
+{{/global}}
+// clang-format on
+
+struct State {
+    uint8_t mem[sizeof(ST7735)];
+    ST7735* dev;
+};
+
+using Type = ST7735*;
+
+// clang-format off
+{{ GENERATED_CODE }}
+// clang-format on
+
+void evaluate(Context ctx) {
+    if (!isSettingUp())
+        return;
+
+    auto state = getState(ctx);
+
+    const uint8_t cs = getValue<input_CS>(ctx);
+    const uint8_t dc = getValue<input_DC>(ctx);
+    const uint8_t rst = getValue<input_RST>(ctx);
+
+    state->dev = new (state->mem) ST7735(cs, dc, rst);
+
+    emitValue<output_DEV>(ctx, state->dev);
+}
+
+template <uint8_t cs, uint8_t dc, uint8_t rst>
+void evaluateTmpl(Context ctx) {
+    static_assert(isValidDigitalPort(cs), "must be a valid digital port");
+    static_assert(isValidDigitalPort(dc), "must be a valid digital port");
+    static_assert(isValidDigitalPort(rst), "must be a valid digital port");
+    evaluate(ctx);
+}
