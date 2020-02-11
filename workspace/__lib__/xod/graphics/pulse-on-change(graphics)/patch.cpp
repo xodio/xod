371300e2288a57b6504e309ab7d1b517b9d78123
gabbapeople <gabbapeople@mail.ru>
2020-02-11 16:41:33 +0300

    feat(stdlib) : core, pulse on change graphics specialization

---
+++ workspace/__lib__/xod/graphics/pulse-on-change(graphics)/patch.cpp
@@ -0,0 +1,11 @@
+struct State {
+};
+
+{{ GENERATED_CODE }}
+
+void evaluate(Context ctx) {
+    //Change in at least one of the elements in the graphical tree is a signal that the whole tree has changed.
+
+    if (isInputDirty<input_IN>(ctx))
+        emitValue<output_OUT>(ctx, 1);
+}
