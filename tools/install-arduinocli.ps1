bbe266a6418d65a132b3e6e0e848356ba33e0ebc
Quin Kennedy <quin@fb.com>
2020-05-15 09:08:23 -0700

    fix(circle-ci): downloading new arduino-cli

--- tools/install-arduinocli.ps1
+++ tools/install-arduinocli.ps1
@@ -1,8 +1,7 @@
 # Install arduino-cli
 
-Invoke-RestMethod -Uri "http://downloads.arduino.cc/PR/arduino-cli/arduino-cli-25-PR91-windows.zip" -Method GET -OutFile "$env:HOME/arduino-cli.zip"
+Invoke-RestMethod -Uri "https://github.com/arduino/arduino-cli/releases/download/0.10.0/arduino-cli_0.10.0_Windows_64bit.zip" -Method GET -OutFile "$env:HOME/arduino-cli.zip"
 unzip "$env:HOME/arduino-cli.zip" -d "$env:HOME"
-mv "$env:HOME/arduino-cli-25-PR91-windows.exe" "$env:HOME/arduino-cli.exe"
 copy "$env:HOME/arduino-cli.exe" "./packages/xod-client-electron/arduino-cli.exe"
 
 $env:XOD_ARDUINO_CLI="$env:HOME/arduino-cli.exe"
