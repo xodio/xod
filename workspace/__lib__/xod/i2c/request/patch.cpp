ee2780d8a95e2b815c03efc76d321105b217f06b
Kirill Shumilov <brusher.designer@gmail.com>
2020-08-18 15:01:54 +0300

    chore(stdlib): update `xod/i2c`

--- workspace/__lib__/xod/i2c/request/patch.cpp
+++ workspace/__lib__/xod/i2c/request/patch.cpp
@@ -2,24 +2,21 @@
 #pragma XOD evaluate_on_pin enable input_SEND
 #pragma XOD error_raise enable
 
-struct State {
-};
+node {
+    void evaluate(Context ctx) {
+        if (!isInputDirty<input_SEND>(ctx))
+            return;
 
-{{ GENERATED_CODE }}
+        auto wire = getValue<input_I2C>(ctx);
+        auto addr = (uint8_t)getValue<input_ADDR>(ctx);
+        auto nBytes = (uint8_t)getValue<input_N>(ctx);
 
-void evaluate(Context ctx) {
-    if (!isInputDirty<input_SEND>(ctx))
-        return;
+        if (addr > 127) {
+            raiseError(ctx); // Invalid I2C address
+            return;
+        }
 
-    auto wire = getValue<input_I2C>(ctx);
-    auto addr = (uint8_t)getValue<input_ADDR>(ctx);
-    auto nBytes = (uint8_t)getValue<input_N>(ctx);
-
-    if (addr > 127) {
-        raiseError(ctx); // Invalid I2C address
-        return;
+        wire->requestFrom(addr, nBytes);
+        emitValue<output_DONE>(ctx, 1);
     }
-
-    wire->requestFrom(addr, nBytes);
-    emitValue<output_DONE>(ctx, 1);
 }
