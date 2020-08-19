a73f950997e7ec3d013e1798b19a99d7053badc0
Kirill Shumilov <brusher.designer@gmail.com>
2020-08-19 17:28:06 +0300

    chore(stdlib): update `xod/json`

--- workspace/__lib__/xod/json/nth/patch.cpp
+++ workspace/__lib__/xod/json/nth/patch.cpp
@@ -1,45 +1,45 @@
 #pragma XOD evaluate_on_pin disable
 #pragma XOD evaluate_on_pin enable input_IN2
-using ParserState = xod::json_parser::ParserState;
 
-struct State {
+nodespace {
+    using ParserState = xod::json_parser::ParserState;
+}
+
+node {
     uint16_t depth = 0;
     uint16_t currentIndex = 0;
-};
 
-{{ GENERATED_CODE }}
+    void evaluate(Context ctx) {
+        if (!isInputDirty<input_IN2>(ctx)) return;
 
-void evaluate(Context ctx) {
-    if (!isInputDirty<input_IN2>(ctx)) return;
+        auto desiredIndex = getValue<input_N>(ctx);
+        auto parsed = getValue<input_IN1>(ctx);
 
-    auto state = getState(ctx);
-    auto desiredIndex = getValue<input_N>(ctx);
-    auto parsed = getValue<input_IN1>(ctx);
+        if (parsed.state == ParserState::START_ARRAY && depth == 0)
+            currentIndex = 0;
 
-    if (parsed.state == ParserState::START_ARRAY && state->depth == 0)
-        state->currentIndex = 0;
+        if (parsed.state == ParserState::START_OBJECT || parsed.state == ParserState::START_ARRAY)
+            depth++;
 
-    if (parsed.state == ParserState::START_OBJECT || parsed.state == ParserState::START_ARRAY)
-        state->depth++;
+        bool shouldDecreaseDepth = parsed.state == ParserState::AFTER_OBJECT || parsed.state == ParserState::AFTER_ARRAY || parsed.state == ParserState::DONE;
+        // trailing ']' and '}' that are passed to indicate end of the value should not decrease depth
+        if (shouldDecreaseDepth && depth > 0)
+            depth--;
 
-    bool shouldDecreaseDepth = parsed.state == ParserState::AFTER_OBJECT || parsed.state == ParserState::AFTER_ARRAY || parsed.state == ParserState::DONE;
-    // trailing ']' and '}' that are passed to indicate end of the value should not decrease depth
-    if (shouldDecreaseDepth && state->depth > 0)
-        state->depth--;
+        // do not pass opening '['
+        if (parsed.state == ParserState::START_ARRAY && depth == 1)
+            return;
 
-    // do not pass opening '['
-    if (parsed.state == ParserState::START_ARRAY && state->depth == 1)
-        return;
+        if (desiredIndex == currentIndex) {
+            emitValue<output_OUT1>(ctx, parsed);
+            emitValue<output_OUT2>(ctx, true);
+        }
 
-    if (desiredIndex == state->currentIndex) {
-        emitValue<output_OUT1>(ctx, parsed);
-        emitValue<output_OUT2>(ctx, true);
-    }
-
-    bool valueHasEnded = state->depth == 1 && parsed.state == ParserState::IN_ARRAY;
-    bool arrayHasEnded = state->depth == 0 && parsed.state == ParserState::AFTER_ARRAY;
-    if (valueHasEnded || arrayHasEnded) {
-        state->currentIndex++;
-        return;
+        bool valueHasEnded = depth == 1 && parsed.state == ParserState::IN_ARRAY;
+        bool arrayHasEnded = depth == 0 && parsed.state == ParserState::AFTER_ARRAY;
+        if (valueHasEnded || arrayHasEnded) {
+            currentIndex++;
+            return;
+        }
     }
 }
