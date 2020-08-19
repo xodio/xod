f021b58cded6756cc606e3dd66655be54be8b855
Kirill Shumilov <brusher.designer@gmail.com>
2020-08-19 16:07:06 +0300

    chore(stdlib): update `xod/bits`

--- workspace/__lib__/xod/bits/bitwise-or/patch.cpp
+++ workspace/__lib__/xod/bits/bitwise-or/patch.cpp
@@ -1,13 +1,9 @@
-
 #pragma XOD dirtieness disable
 
-struct State {
-};
-
-{{ GENERATED_CODE }}
-
-void evaluate(Context ctx) {
-    auto in1 = getValue<input_IN1>(ctx);
-    auto in2 = getValue<input_IN2>(ctx);
-    emitValue<output_OUT>(ctx, in1 | in2);
+node {
+    void evaluate(Context ctx) {
+        auto in1 = getValue<input_IN1>(ctx);
+        auto in2 = getValue<input_IN2>(ctx);
+        emitValue<output_OUT>(ctx, in1 | in2);
+    }
 }
--- workspace/__lib__/xod/bits/i16-to-number/patch.cpp
+++ workspace/__lib__/xod/bits/i16-to-number/patch.cpp
@@ -1,13 +1,9 @@
-
-struct State {
-};
-
-{{ GENERATED_CODE }}
-
-void evaluate(Context ctx) {
-    uint8_t b1 = getValue<input_B1>(ctx);
-    uint8_t b0 = getValue<input_B0>(ctx);
-    int16_t bytes = ((b1 << 8) | b0);
-    Number result = (Number)bytes;
-    emitValue<output_OUT>(ctx, result);
+node {
+    void evaluate(Context ctx) {
+        uint8_t b1 = getValue<input_B1>(ctx);
+        uint8_t b0 = getValue<input_B0>(ctx);
+        int16_t bytes = ((b1 << 8) | b0);
+        Number result = (Number)bytes;
+        emitValue<output_OUT>(ctx, result);
+    }
 }
