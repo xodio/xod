f4a23e5b3e39949868fae44e2c06fa23b6042dbf
Kirill Shumilov <brusher.designer@gmail.com>
2020-08-19 17:28:06 +0300

    chore(stdlib): update `xod/json`

--- workspace/__lib__/xod/json/boolean-value/patch.cpp
+++ workspace/__lib__/xod/json/boolean-value/patch.cpp
@@ -1,28 +1,28 @@
 #pragma XOD evaluate_on_pin disable
 #pragma XOD evaluate_on_pin enable input_IN2
-using ParserState = xod::json_parser::ParserState;
 
-struct State {
+nodespace {
+    using ParserState = xod::json_parser::ParserState;
+}
+
+node {
     ParserState prevParsedState;
-};
 
-{{ GENERATED_CODE }}
+    void evaluate(Context ctx) {
+        if (!isInputDirty<input_IN2>(ctx)) return;
 
-void evaluate(Context ctx) {
-    if (!isInputDirty<input_IN2>(ctx)) return;
+        auto parsed = getValue<input_IN1>(ctx);
 
-    auto state = getState(ctx);
-    auto parsed = getValue<input_IN1>(ctx);
-
-    if (parsed.state != state->prevParsedState) {
-        if (state->prevParsedState == ParserState::IN_TRUE) {
-            emitValue<output_OUT>(ctx, true);
-            emitValue<output_DONE>(ctx, 1);
-        } else if (state->prevParsedState == ParserState::IN_FALSE) {
-            emitValue<output_OUT>(ctx, false);
-            emitValue<output_DONE>(ctx, 1);
+        if (parsed.state != prevParsedState) {
+            if (prevParsedState == ParserState::IN_TRUE) {
+                emitValue<output_OUT>(ctx, true);
+                emitValue<output_DONE>(ctx, 1);
+            } else if (prevParsedState == ParserState::IN_FALSE) {
+                emitValue<output_OUT>(ctx, false);
+                emitValue<output_DONE>(ctx, 1);
+            }
         }
-    }
 
-    state->prevParsedState = parsed.state;
+        prevParsedState = parsed.state;
+    }
 }
--- workspace/__lib__/xod/json/number-value/patch.cpp
+++ workspace/__lib__/xod/json/number-value/patch.cpp
@@ -1,45 +1,45 @@
 #pragma XOD evaluate_on_pin disable
 #pragma XOD evaluate_on_pin enable input_IN2
-using ParserState = xod::json_parser::ParserState;
 
-struct State {
+nodespace {
+    using ParserState = xod::json_parser::ParserState;
+}
+
+node {
     uint32_t integerResult = 0;
     uint32_t divider = 1;
     int8_t sign = 1;
     bool isParsingDecimalPart = false;
     ParserState prevParsedState;
-};
 
-{{ GENERATED_CODE }}
+    void evaluate(Context ctx) {
+        auto parsed = getValue<input_IN1>(ctx);
 
-void evaluate(Context ctx) {
-    auto state = getState(ctx);
-    auto parsed = getValue<input_IN1>(ctx);
+        if (!isInputDirty<input_IN2>(ctx)) return;
 
-    if (!isInputDirty<input_IN2>(ctx)) return;
+        if (parsed.state == ParserState::IN_NUMBER) {
+            if (parsed.character == '-') {
+               sign = -1;
+            } else if (parsed.character == '.') {
+                isParsingDecimalPart = true;
+            } else if (parsed.character >= '0' && parsed.character <= '9') {
+                integerResult *= 10;
+                integerResult += parsed.character - '0';
 
-    if (parsed.state == ParserState::IN_NUMBER) {
-        if (parsed.character == '-') {
-           state->sign = -1;
-        } else if (parsed.character == '.') {
-            state->isParsingDecimalPart = true;
-        } else if (parsed.character >= '0' && parsed.character <= '9') {
-            state->integerResult *= 10;
-            state->integerResult += parsed.character - '0';
+                if (isParsingDecimalPart)
+                    divider *= 10;
+            }
+        } else if (prevParsedState == ParserState::IN_NUMBER) {
+            Number result = (Number)integerResult / (Number)divider * (Number)sign;
+            emitValue<output_OUT>(ctx, result);
+            emitValue<output_DONE>(ctx, 1);
 
-            if (state->isParsingDecimalPart)
-                state->divider *= 10;
+            integerResult = 0;
+            divider = 1;
+            sign = 1;
+            isParsingDecimalPart = false;
         }
-    } else if (state->prevParsedState == ParserState::IN_NUMBER) {
-        Number result = (Number)state->integerResult / (Number)state->divider * (Number)state->sign;
-        emitValue<output_OUT>(ctx, result);
-        emitValue<output_DONE>(ctx, 1);
 
-        state->integerResult = 0;
-        state->divider = 1;
-        state->sign = 1;
-        state->isParsingDecimalPart = false;
+        prevParsedState = parsed.state;
     }
-
-    state->prevParsedState = parsed.state;
 }
