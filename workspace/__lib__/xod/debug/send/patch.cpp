dfcc503c57cda1cd427151456ce4281940d90d49
Kirill Shumilov <brusher.designer@gmail.com>
2020-08-19 19:53:15 +0300

    chore(stdlib): update `xod/debug` (without tweak-strings)

--- workspace/__lib__/xod/debug/send/patch.cpp
+++ workspace/__lib__/xod/debug/send/patch.cpp
@@ -1,26 +1,21 @@
 #pragma XOD evaluate_on_pin disable
 #pragma XOD evaluate_on_pin enable input_SEND
 
-struct State {
-};
+node {
+    void evaluate(Context ctx) {
+        if (!isInputDirty<input_SEND>(ctx))
+            return;
 
-// clang-format off
-{{ GENERATED_CODE }}
-// clang-format on
+        auto socket = getValue<input_SOCK>(ctx);
+        auto req = getValue<input_MSG>(ctx);
+        auto inet = getValue<input_INET>(ctx);
 
-void evaluate(Context ctx) {
-    if (!isInputDirty<input_SEND>(ctx))
-        return;
-
-    auto socket = getValue<input_SOCK>(ctx);
-    auto req = getValue<input_MSG>(ctx);
-    auto inet = getValue<input_INET>(ctx);
-
-    emitValue<output_INETU0027>(ctx, inet);
-    emitValue<output_SOCKU0027>(ctx, socket);
-    if (inet->send(req)) {
-        emitValue<output_DONE>(ctx, 1);
-    } else {
-        raiseError(ctx);
+        emitValue<output_INETU0027>(ctx, inet);
+        emitValue<output_SOCKU0027>(ctx, socket);
+        if (inet->send(req)) {
+            emitValue<output_DONE>(ctx, 1);
+        } else {
+            raiseError(ctx);
+        }
     }
 }
--- workspace/__lib__/xod/debug/watch/patch.cpp
+++ workspace/__lib__/xod/debug/watch/patch.cpp
@@ -2,32 +2,29 @@
 #pragma XOD evaluate_on_pin enable input_IN
 #pragma XOD error_catch enable
 
-struct State {
-};
+node {
+    void evaluate(Context ctx) {
+      if (!isInputDirty<input_IN>(ctx))
+          return;
 
-{{ GENERATED_CODE }}
+      auto err = getError<input_IN>(ctx);
+      auto line = getValue<input_IN>(ctx);
 
-void evaluate(Context ctx) {
-  if (!isInputDirty<input_IN>(ctx))
-      return;
+      TimeMs tNow = transactionTime();
+      XOD_DEBUG_SERIAL.print(F("+XOD:"));
+      XOD_DEBUG_SERIAL.print(tNow);
+      XOD_DEBUG_SERIAL.print(':');
+      XOD_DEBUG_SERIAL.print(getNodeId(ctx));
+      XOD_DEBUG_SERIAL.print(':');
+      if (err) {
+        XOD_DEBUG_SERIAL.print(F("ERR"));
+      } else {
+        for (auto it = line.iterate(); it; ++it)
+          XOD_DEBUG_SERIAL.print((char)*it);
+      }
 
-  auto err = getError<input_IN>(ctx);
-  auto line = getValue<input_IN>(ctx);
-
-  TimeMs tNow = transactionTime();
-  XOD_DEBUG_SERIAL.print(F("+XOD:"));
-  XOD_DEBUG_SERIAL.print(tNow);
-  XOD_DEBUG_SERIAL.print(':');
-  XOD_DEBUG_SERIAL.print(getNodeId(ctx));
-  XOD_DEBUG_SERIAL.print(':');
-  if (err) {
-    XOD_DEBUG_SERIAL.print(F("ERR"));
-  } else {
-    for (auto it = line.iterate(); it; ++it)
-      XOD_DEBUG_SERIAL.print((char)*it);
-  }
-
-  XOD_DEBUG_SERIAL.print('\r');
-  XOD_DEBUG_SERIAL.print('\n');
-  XOD_DEBUG_SERIAL.flush();
+      XOD_DEBUG_SERIAL.print('\r');
+      XOD_DEBUG_SERIAL.print('\n');
+      XOD_DEBUG_SERIAL.flush();
+    }
 }
