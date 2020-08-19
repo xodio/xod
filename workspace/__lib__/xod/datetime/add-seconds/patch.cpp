80a349e557597b59093e62481cf2c182024335af
Kirill Shumilov <brusher.designer@gmail.com>
2020-08-19 15:54:50 +0300

    chore(stdlib): update `xod/datetime`

--- workspace/__lib__/xod/datetime/add-seconds/patch.cpp
+++ workspace/__lib__/xod/datetime/add-seconds/patch.cpp
@@ -1,13 +1,7 @@
-
-struct State {
-};
-
-// clang-format off
-{{ GENERATED_CODE }}
-// clang-format on
-
-void evaluate(Context ctx) {
-    auto inValue = getValue<input_IN>(ctx);
-    int32_t sec = getValue<input_T>(ctx);
-    emitValue<output_OUT>(ctx, inValue + sec);
+node {
+    void evaluate(Context ctx) {
+        auto inValue = getValue<input_IN>(ctx);
+        int32_t sec = getValue<input_T>(ctx);
+        emitValue<output_OUT>(ctx, inValue + sec);
+    }
 }
