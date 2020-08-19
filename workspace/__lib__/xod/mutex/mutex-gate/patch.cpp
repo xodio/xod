f6590fba5274cc3f794615864ad4d43ffeee53ec
Kirill Shumilov <brusher.designer@gmail.com>
2020-08-19 18:42:30 +0300

    chore(stdlib): update `xod/mutex`

--- workspace/__lib__/xod/mutex/mutex-gate/patch.cpp
+++ workspace/__lib__/xod/mutex/mutex-gate/patch.cpp
@@ -1,32 +1,28 @@
+node {
+    void evaluate(Context ctx) {
+        auto mux = getValue<input_MUX>(ctx);
 
-struct State {
-};
+        if (isSettingUp()) {
+            // Short-circuit RES and RES'
+            emitValue<output_MUXU0027>(ctx, mux);
+        }
 
-{{ GENERATED_CODE }}
+        bool justEntered = false;
+        auto nodeId = getNodeId(ctx);
 
-void evaluate(Context ctx) {
-    auto mux = getValue<input_MUX>(ctx);
+        if (isInputDirty<input_ACQ>(ctx) && mux->lockFor(nodeId)) {
+            justEntered = true;
+        }
 
-    if (isSettingUp()) {
-        // Short-circuit RES and RES'
-        emitValue<output_MUXU0027>(ctx, mux);
+        if (isInputDirty<input_RLS>(ctx) && mux->unlock(nodeId)) {
+            justEntered = false;
+            emitValue<output_RLSd>(ctx, 1);
+        }
+
+        if (justEntered)
+            emitValue<output_ACQd>(ctx, 1);
+
+        if (isInputDirty<input_IN>(ctx) && mux->isLockedFor(nodeId))
+            emitValue<output_OUT>(ctx, 1);
     }
-
-    bool justEntered = false;
-    auto nodeId = getNodeId(ctx);
-
-    if (isInputDirty<input_ACQ>(ctx) && mux->lockFor(nodeId)) {
-        justEntered = true;
-    }
-
-    if (isInputDirty<input_RLS>(ctx) && mux->unlock(nodeId)) {
-        justEntered = false;
-        emitValue<output_RLSd>(ctx, 1);
-    }
-
-    if (justEntered)
-        emitValue<output_ACQd>(ctx, 1);
-
-    if (isInputDirty<input_IN>(ctx) && mux->isLockedFor(nodeId))
-        emitValue<output_OUT>(ctx, 1);
 }
