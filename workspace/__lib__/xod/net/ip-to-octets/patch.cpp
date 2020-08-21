b33ef722df6cc04fad4b6d30fa8de5530b62a73a
Kirill Shumilov <brusher.designer@gmail.com>
2020-08-21 14:52:09 +0300

    chore(stdlib): update `xod/net`

--- workspace/__lib__/xod/net/ip-to-octets/patch.cpp
+++ workspace/__lib__/xod/net/ip-to-octets/patch.cpp
@@ -1,16 +1,12 @@
+node {
+    void evaluate(Context ctx) {
+        auto ip = getValue<input_IN>(ctx);
 
-struct State {
-};
+        auto octets = reinterpret_cast<uint8_t*>(&ip);
 
-{{ GENERATED_CODE }}
-
-void evaluate(Context ctx) {
-    auto ip = getValue<input_IN>(ctx);
-
-    auto octets = reinterpret_cast<uint8_t*>(&ip);
-
-    emitValue<output_OUT1>(ctx, octets[0]);
-    emitValue<output_OUT2>(ctx, octets[1]);
-    emitValue<output_OUT3>(ctx, octets[2]);
-    emitValue<output_OUT4>(ctx, octets[3]);
+        emitValue<output_OUT1>(ctx, octets[0]);
+        emitValue<output_OUT2>(ctx, octets[1]);
+        emitValue<output_OUT3>(ctx, octets[2]);
+        emitValue<output_OUT4>(ctx, octets[3]);
+    }
 }
--- workspace/__lib__/xod/net/mac-address/patch.cpp
+++ workspace/__lib__/xod/net/mac-address/patch.cpp
@@ -1,21 +1,18 @@
+node {
+    meta {
+        using Type = uint8_t*;
+    }
 
-struct State {
     uint8_t mac[6];
-};
 
-using Type = uint8_t*;
+    void evaluate(Context ctx) {
+        mac[0] = getValue<input_IN1>(ctx);
+        mac[1] = getValue<input_IN2>(ctx);
+        mac[2] = getValue<input_IN3>(ctx);
+        mac[3] = getValue<input_IN4>(ctx);
+        mac[4] = getValue<input_IN5>(ctx);
+        mac[5] = getValue<input_IN6>(ctx);
 
-{{ GENERATED_CODE }}
-
-void evaluate(Context ctx) {
-    auto state = getState(ctx);
-
-    state->mac[0] = getValue<input_IN1>(ctx);
-    state->mac[1] = getValue<input_IN2>(ctx);
-    state->mac[2] = getValue<input_IN3>(ctx);
-    state->mac[3] = getValue<input_IN4>(ctx);
-    state->mac[4] = getValue<input_IN5>(ctx);
-    state->mac[5] = getValue<input_IN6>(ctx);
-
-    emitValue<output_OUT>(ctx, state->mac);
+        emitValue<output_OUT>(ctx, mac);
+    }
 }
