2e0b4c9d21a8497c8df19eed70859ee8005989c4
Kirill Shumilov <brusher.designer@gmail.com>
2020-08-21 18:27:28 +0300

    chore(stdlib): update `xod/color`

--- workspace/__lib__/xod/color/format-color/patch.cpp
+++ workspace/__lib__/xod/color/format-color/patch.cpp
@@ -1,30 +1,24 @@
 
-struct State {
+node {
     char str[7] = { 0, 0, 0, 0, 0, 0, '\0' };
-    CStringView view;
-    State() : view(str) { }
-};
+    CStringView view = CStringView(str);
 
-// clang-format off
-{{ GENERATED_CODE }}
-// clang-format on
+    char nibbleToChar(uint8_t nibble) {
+      return (nibble < 10)
+        ? '0' + nibble
+        : 'A' + nibble - 10;
+    }
 
-char nibbleToChar(uint8_t nibble) {
-  return (nibble < 10)
-    ? '0' + nibble
-    : 'A' + nibble - 10;
-}
+    void evaluate(Context ctx) {
+        auto color = getValue<input_IN>(ctx);
 
-void evaluate(Context ctx) {
-    auto state = getState(ctx);
-    auto color = getValue<input_IN>(ctx);
+        str[0] = nibbleToChar(color.r >> 4);
+        str[1] = nibbleToChar(color.r & 0x0F);
+        str[2] = nibbleToChar(color.g >> 4);
+        str[3] = nibbleToChar(color.g & 0x0F);
+        str[4] = nibbleToChar(color.b >> 4);
+        str[5] = nibbleToChar(color.b & 0x0F);
 
-    state->str[0] = nibbleToChar(color.r >> 4);
-    state->str[1] = nibbleToChar(color.r & 0x0F);
-    state->str[2] = nibbleToChar(color.g >> 4);
-    state->str[3] = nibbleToChar(color.g & 0x0F);
-    state->str[4] = nibbleToChar(color.b >> 4);
-    state->str[5] = nibbleToChar(color.b & 0x0F);
-
-    emitValue<output_OUT>(ctx, XString(&state->view));
+        emitValue<output_OUT>(ctx, XString(&view));
+    }
 }
--- workspace/__lib__/xod/color/to-hsl/patch.cpp
+++ workspace/__lib__/xod/color/to-hsl/patch.cpp
@@ -1,52 +1,45 @@
+node {
+    void evaluate(Context ctx)
+    {
 
-struct State {
-};
+        auto color = getValue<input_IN>(ctx);
+        float r = color.r / 255.0f;
+        float g = color.g / 255.0f;
+        float b = color.b / 255.0f;
 
-// clang-format off
-{{ GENERATED_CODE }}
-// clang-format on
+        float _min = min(min(r, g), b);
+        float _max = max(max(r, g), b);
+        float delta = _max - _min;
 
-void evaluate(Context ctx)
-{
+        float l, s;
+        float hue;
 
-    auto color = getValue<input_IN>(ctx);
-    float r = color.r / 255.0f;
-    float g = color.g / 255.0f;
-    float b = color.b / 255.0f;
+        l = (_max + _min) / 2;
 
-    float _min = min(min(r, g), b);
-    float _max = max(max(r, g), b);
-    float delta = _max - _min;
-
-    float l, s;
-    float hue;
-
-    l = (_max + _min) / 2;
-
-    if (delta == 0) {
-        hue = 0;
-        s = 0;
-    }
-    else {
-        s = (l <= 0.5) ? (delta / (_max + _min)) : (delta / (2 - _max - _min));
-
-        if (r == _max) {
-            hue = ((g - b) / 6) / delta;
-        }
-        else if (g == _max) {
-            hue = (1.0f / 3) + ((b - r) / 6) / delta;
+        if (delta == 0) {
+            hue = 0;
+            s = 0;
         }
         else {
-            hue = (2.0f / 3) + ((r - g) / 6) / delta;
+            s = (l <= 0.5) ? (delta / (_max + _min)) : (delta / (2 - _max - _min));
+
+            if (r == _max) {
+                hue = ((g - b) / 6) / delta;
+            }
+            else if (g == _max) {
+                hue = (1.0f / 3) + ((b - r) / 6) / delta;
+            }
+            else {
+                hue = (2.0f / 3) + ((r - g) / 6) / delta;
+            }
+
+            if (hue < 0)
+                hue += 1;
+            if (hue > 1)
+                hue -= 1;
         }
-
-        if (hue < 0)
-            hue += 1;
-        if (hue > 1)
-            hue -= 1;
+        emitValue<output_H>(ctx, hue);
+        emitValue<output_S>(ctx, s);
+        emitValue<output_L>(ctx, l);
     }
-    emitValue<output_H>(ctx, hue);
-    emitValue<output_S>(ctx, s);
-    emitValue<output_L>(ctx, l);
 }
-
