5e9ff7939aabb08cff30f82d9bed4d10e56b2b8f
Kirill Shumilov <brusher.designer@gmail.com>
2020-08-21 18:27:28 +0300

    chore(stdlib): update `xod/color`

--- workspace/__lib__/xod/color/color-rgb-bytes/patch.cpp
+++ workspace/__lib__/xod/color/color-rgb-bytes/patch.cpp
@@ -1,13 +1,7 @@
-
-struct State {
-};
-
-// clang-format off
-{{ GENERATED_CODE }}
-// clang-format on
-
-void evaluate(Context ctx) {
-    ValueType<output_OUT>::T obj;
-    obj = { (uint8_t)getValue<input_R>(ctx), (uint8_t)getValue<input_G>(ctx), (uint8_t)getValue<input_B>(ctx) };
-    emitValue<output_OUT>(ctx, obj);
+node {
+    void evaluate(Context ctx) {
+        typeof_OUT obj;
+        obj = { (uint8_t)getValue<input_R>(ctx), (uint8_t)getValue<input_G>(ctx), (uint8_t)getValue<input_B>(ctx) };
+        emitValue<output_OUT>(ctx, obj);
+    }
 }
