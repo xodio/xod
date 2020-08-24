d7ab608308bf1831e78cc97ec902143217c5cfa1
Kirill Shumilov <brusher.designer@gmail.com>
2020-08-24 13:23:29 +0300

    chore(stdlib): update `xod-dev/dht`

--- workspace/__lib__/xod-dev/dht/dht11-device/patch.cpp
+++ workspace/__lib__/xod-dev/dht/dht11-device/patch.cpp
@@ -1,12 +1,14 @@
-
 node {
     meta {
-        struct Dht11Device {
-            static constexpr uint8_t port = constant_input_PORT;
-        };
-
-        using Type = Dht11Device*;
+      struct Type {
+         static constexpr typeof_PORT port = constant_input_PORT;
+      };
     }
 
-    void evaluate(Context ctx) {}
+    void evaluate(Context ctx) {
+        static_assert(isValidDigitalPort(constant_input_PORT), "must be a valid digital port");
+
+        // just to trigger downstream nodes
+        emitValue<output_DEV>(ctx, {});
+    }
 }
