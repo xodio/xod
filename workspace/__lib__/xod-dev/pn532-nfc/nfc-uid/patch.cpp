14ca2264fbf994986c0bd9381edc8ad895f8527e
Kirill Shumilov <brusher.designer@gmail.com>
2020-08-26 17:49:02 +0300

    chore(stdlib): update `xod-dev/pn532-nfc`

--- workspace/__lib__/xod-dev/pn532-nfc/nfc-uid/patch.cpp
+++ workspace/__lib__/xod-dev/pn532-nfc/nfc-uid/patch.cpp
@@ -1,32 +1,27 @@
-struct State {
-};
+node {
+    meta {
+        struct Type {
+            uint8_t items[7];
 
-struct TagUID {
-    uint8_t items[7];
+            static Type empty() {
+                Type emptyUid;
+                memset(emptyUid.items, 0, 7);
+                return emptyUid;
+            };
 
-    static TagUID empty() {
-        TagUID emptyUid;
-        memset(emptyUid.items, 0, 7);
-        return emptyUid;
-    };
+        };
+    }
 
-};
+    void evaluate(Context ctx) {
+        Type uid;
+        uid.items[0] = (uint8_t)getValue<input_IN1>(ctx);
+        uid.items[1] = (uint8_t)getValue<input_IN2>(ctx);
+        uid.items[2] = (uint8_t)getValue<input_IN3>(ctx);
+        uid.items[3] = (uint8_t)getValue<input_IN4>(ctx);
+        uid.items[4] = (uint8_t)getValue<input_IN5>(ctx);
+        uid.items[5] = (uint8_t)getValue<input_IN6>(ctx);
+        uid.items[6] = (uint8_t)getValue<input_IN7>(ctx);
 
-using Type = TagUID;
-
-// clang-format off
-{{ GENERATED_CODE }}
-// clang-format on
-
-void evaluate(Context ctx) {
-    TagUID uid;
-    uid.items[0] = (uint8_t)getValue<input_IN1>(ctx);
-    uid.items[1] = (uint8_t)getValue<input_IN2>(ctx);
-    uid.items[2] = (uint8_t)getValue<input_IN3>(ctx);
-    uid.items[3] = (uint8_t)getValue<input_IN4>(ctx);
-    uid.items[4] = (uint8_t)getValue<input_IN5>(ctx);
-    uid.items[5] = (uint8_t)getValue<input_IN6>(ctx);
-    uid.items[6] = (uint8_t)getValue<input_IN7>(ctx);
-
-    emitValue<output_UID>(ctx, uid);
+        emitValue<output_UID>(ctx, uid);
+    }
 }
--- workspace/__lib__/xod-dev/pn532-nfc/pair-tag/patch.cpp
+++ workspace/__lib__/xod-dev/pn532-nfc/pair-tag/patch.cpp
@@ -1,31 +1,26 @@
 #pragma XOD evaluate_on_pin disable
 #pragma XOD evaluate_on_pin enable input_PAIR
 
-struct State {
-};
+node {
+    void evaluate(Context ctx) {
+        if (!isInputDirty<input_PAIR>(ctx))
+            return;
 
-// clang-format off
-{{ GENERATED_CODE }}
-// clang-format on
+        auto nfc = getValue<input_DEV>(ctx);
 
-void evaluate(Context ctx) {
-    if (!isInputDirty<input_PAIR>(ctx))
-        return;
+        uint8_t uidLength;
+        uint8_t readedUid[12];
+        bool res = nfc->readPassiveTargetID(PN532_MIFARE_ISO14443A, readedUid, &uidLength);
 
-    auto nfc = getValue<input_DEV>(ctx);
-
-    uint8_t uidLength;
-    uint8_t readedUid[12];
-    bool res = nfc->readPassiveTargetID(PN532_MIFARE_ISO14443A, readedUid, &uidLength);
-
-    if (res) {
-        ValueType<output_UID>::T uid;
-        memset(readedUid + uidLength, 0, 12 - uidLength);
-        memcpy(uid.items, readedUid, 7);
-        emitValue<output_UID>(ctx, uid);
-        emitValue<output_OK>(ctx, 1);
-    } else {
-        emitValue<output_UID>(ctx, ValueType<output_UID>::T::empty());
-        emitValue<output_NA>(ctx, 1);
+        if (res) {
+            typeof_UID uid;
+            memset(readedUid + uidLength, 0, 12 - uidLength);
+            memcpy(uid.items, readedUid, 7);
+            emitValue<output_UID>(ctx, uid);
+            emitValue<output_OK>(ctx, 1);
+        } else {
+            emitValue<output_UID>(ctx, remove_pointer<typeof_UID>::type::empty());
+            emitValue<output_NA>(ctx, 1);
+        }
     }
 }
