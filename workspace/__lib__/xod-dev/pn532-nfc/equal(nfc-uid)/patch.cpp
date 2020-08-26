46efb4b744ddd5ec24be3a92cd70763d302ecd43
Kirill Shumilov <brusher.designer@gmail.com>
2020-08-26 17:49:02 +0300

    chore(stdlib): update `xod-dev/pn532-nfc`

--- workspace/__lib__/xod-dev/pn532-nfc/equal(nfc-uid)/patch.cpp
+++ workspace/__lib__/xod-dev/pn532-nfc/equal(nfc-uid)/patch.cpp
@@ -1,13 +1,8 @@
-struct State {
-};
-
-// clang-format off
-{{ GENERATED_CODE }}
-// clang-format on
-
-void evaluate(Context ctx) {
-    auto uidA = getValue<input_IN1>(ctx);
-    auto uidB = getValue<input_IN2>(ctx);
-    bool eq = memcmp(uidA.items, uidB.items, sizeof(uidA.items)) == 0;
-    emitValue<output_OUT>(ctx, eq);
+node {
+    void evaluate(Context ctx) {
+        auto uidA = getValue<input_IN1>(ctx);
+        auto uidB = getValue<input_IN2>(ctx);
+        bool eq = memcmp(uidA.items, uidB.items, sizeof(uidA.items)) == 0;
+        emitValue<output_OUT>(ctx, eq);
+    }
 }
--- workspace/__lib__/xod-dev/pn532-nfc/read-page/patch.cpp
+++ workspace/__lib__/xod-dev/pn532-nfc/read-page/patch.cpp
@@ -2,30 +2,25 @@
 #pragma XOD evaluate_on_pin enable input_READ
 #pragma XOD error_raise enable
 
-struct State {
-};
+node {
+    void evaluate(Context ctx) {
+        if (!isInputDirty<input_READ>(ctx))
+            return;
 
-// clang-format off
-{{ GENERATED_CODE }}
-// clang-format on
+        auto nfc = getValue<input_DEV>(ctx);
+        uint8_t page = (uint8_t)getValue<input_PAGE>(ctx);
 
-void evaluate(Context ctx) {
-    if (!isInputDirty<input_READ>(ctx))
-        return;
+        uint8_t data[4];
+        uint8_t success = nfc->mifareultralight_ReadPage(page, data);
 
-    auto nfc = getValue<input_DEV>(ctx);
-    uint8_t page = (uint8_t)getValue<input_PAGE>(ctx);
-
-    uint8_t data[4];
-    uint8_t success = nfc->mifareultralight_ReadPage(page, data);
-
-    if (success) {
-        emitValue<output_OUT1>(ctx, data[0]);
-        emitValue<output_OUT2>(ctx, data[1]);
-        emitValue<output_OUT3>(ctx, data[2]);
-        emitValue<output_OUT4>(ctx, data[3]);
-        emitValue<output_OK>(ctx, 1);
-    } else {
-        raiseError(ctx);
+        if (success) {
+            emitValue<output_OUT1>(ctx, data[0]);
+            emitValue<output_OUT2>(ctx, data[1]);
+            emitValue<output_OUT3>(ctx, data[2]);
+            emitValue<output_OUT4>(ctx, data[3]);
+            emitValue<output_OK>(ctx, 1);
+        } else {
+            raiseError(ctx);
+        }
     }
 }
