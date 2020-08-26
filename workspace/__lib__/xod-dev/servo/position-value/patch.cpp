8103ce8f77101d346e8c3a9066a0899ae835e59c
Kirill Shumilov <brusher.designer@gmail.com>
2020-08-26 18:00:54 +0300

    chore(stdlib): update `xod-dev/servo`

--- workspace/__lib__/xod-dev/servo/position-value/patch.cpp
+++ workspace/__lib__/xod-dev/servo/position-value/patch.cpp
@@ -1,22 +1,19 @@
 #pragma XOD evaluate_on_pin disable
 #pragma XOD evaluate_on_pin enable input_UPD
 
-struct State {
-};
+node {
+    void evaluate(Context ctx) {
+        auto xservo = getValue<input_DEV>(ctx);
 
-{{ GENERATED_CODE }}
+        if (isSettingUp()) {
+            // Short-circuit DEV and DEV'
+            emitValue<output_DEVU0027>(ctx, xservo);
+        }
 
-void evaluate(Context ctx) {
-    auto xservo = getValue<input_DEV>(ctx);
+        if (!isInputDirty<input_UPD>(ctx))
+            return;
 
-    if (isSettingUp()) {
-        // Short-circuit DEV and DEV'
-        emitValue<output_DEVU0027>(ctx, xservo);
+        emitValue<output_VAL>(ctx, xservo->read01());
+        emitValue<output_DONE>(ctx, 1);
     }
-
-    if (!isInputDirty<input_UPD>(ctx))
-        return;
-
-    emitValue<output_VAL>(ctx, xservo->read01());
-    emitValue<output_DONE>(ctx, 1);
 }
