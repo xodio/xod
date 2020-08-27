cf351654119448b5033810c99cfd96f8fb137e8c
Kirill Shumilov <brusher.designer@gmail.com>
2020-08-27 20:03:32 +0300

    chore(stdlib): update `xod-dev/es2812`

--- workspace/__lib__/xod-dev/ws2812/clear/patch.cpp
+++ workspace/__lib__/xod-dev/ws2812/clear/patch.cpp
@@ -1,14 +1,10 @@
-
-struct State {
-};
-
-{{ GENERATED_CODE }}
-
-void evaluate(Context ctx) {
-    auto dev = getValue<input_DEV>(ctx);
-    if (isInputDirty<input_DO>(ctx)) {
-        dev->fill(0, 0, 0);
-        emitValue<output_DONE>(ctx, 1);
+node {
+    void evaluate(Context ctx) {
+        auto dev = getValue<input_DEV>(ctx);
+        if (isInputDirty<input_DO>(ctx)) {
+            dev->fill(0, 0, 0);
+            emitValue<output_DONE>(ctx, 1);
+        }
+        emitValue<output_DEVU0027>(ctx, dev);
     }
-    emitValue<output_DEVU0027>(ctx, dev);
 }
