3344becb4818b33f0a3409393a8118ed54de39e4
Kirill Shumilov <brusher.designer@gmail.com>
2020-09-07 15:15:59 +0300

    tweak(infra): update version of bundled arduino-cli from 0.10.0 to 0.12.0

--- tools/install-arduinocli.ps1
+++ tools/install-arduinocli.ps1
@@ -1,6 +1,6 @@
 # Install arduino-cli
 
-Invoke-RestMethod -Uri "https://github.com/arduino/arduino-cli/releases/download/0.10.0/arduino-cli_0.10.0_Windows_64bit.zip" -Method GET -OutFile "$env:HOME/arduino-cli.zip"
+Invoke-RestMethod -Uri "https://github.com/arduino/arduino-cli/releases/download/0.12.0/arduino-cli_0.12.0_Windows_64bit.zip" -Method GET -OutFile "$env:HOME/arduino-cli.zip"
 unzip "$env:HOME/arduino-cli.zip" -d "$env:HOME"
 copy "$env:HOME/arduino-cli.exe" "./packages/xod-client-electron/arduino-cli.exe"
 
