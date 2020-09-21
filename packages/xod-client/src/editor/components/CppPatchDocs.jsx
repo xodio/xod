701cc4b9deabe79c754a3168be9a94476a37c1f1
Kirill Shumilov <brusher.designer@gmail.com>
2020-09-21 21:07:25 +0300

    tweak(xod-client): show constants in C++ Cheatshet and also show a warning message for output constants with code example

--- packages/xod-client/src/editor/components/CppPatchDocs.jsx
+++ packages/xod-client/src/editor/components/CppPatchDocs.jsx
@@ -7,6 +7,29 @@ import Icon from 'react-fa';
 
 import { getUtmSiteUrl } from '../../utils/urls';
 
+const getPinLabel = R.compose(cppEscape, XP.getPinLabel);
+
+const formatPinLabel = pin =>
+  [
+    XP.isConstantType(pin.type) ? 'constant_' : '',
+    XP.getPinDirection(pin),
+    '_',
+    getPinLabel(pin),
+  ].join('');
+
+const formatConstantOutputNotice = pin =>
+  XP.isConstantType(pin.type) ? (
+    <span className="constant-notice">
+      <Icon name="warning" /> Must be defined as
+      <code>
+        <span className="keyword">static constexpr </span>
+        <span className="type">typeof_{getPinLabel(pin)} </span>
+        <span className="variable">{formatPinLabel(pin)} </span>
+        =
+      </code>
+    </span>
+  ) : null;
+
 const CppPatchDocs = ({ patch }) => {
   const [inputPins, outputPins] = R.compose(
     R.map(R.sort(R.ascend(XP.getPinOrder))),
@@ -24,9 +47,7 @@ const CppPatchDocs = ({ patch }) => {
           <ul>
             {inputPins.map(pin => (
               <li key={XP.getPinKey(pin)}>
-                <span className="pin-label">
-                  input_{cppEscape(XP.getPinLabel(pin))}
-                </span>
+                <span className="pin-label">{formatPinLabel(pin)}</span>
                 <span className={`pin-type ${XP.getPinType(pin)}`}>
                   {XP.getPinType(pin)}
                 </span>
@@ -41,12 +62,11 @@ const CppPatchDocs = ({ patch }) => {
           <ul>
             {outputPins.map(pin => (
               <li key={XP.getPinKey(pin)}>
-                <span className="pin-label">
-                  output_{cppEscape(XP.getPinLabel(pin))}
-                </span>
+                <span className="pin-label">{formatPinLabel(pin)}</span>
                 <span className={`pin-type ${XP.getPinType(pin)}`}>
                   {XP.getPinType(pin)}
                 </span>
+                {formatConstantOutputNotice(pin)}
               </li>
             ))}
           </ul>
