6c2dd132d3a037ddf5859f33fe84dcc750ef862a
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2020-10-01 16:49:27 +0300

    fix(stdlib): fix esp8266-mcu/lan-ip implementation

--- workspace/__lib__/xod-dev/esp8266-mcu/lan-ip/patch.cpp
+++ workspace/__lib__/xod-dev/esp8266-mcu/lan-ip/patch.cpp
@@ -1,6 +1,6 @@
 node {
     void evaluate(Context ctx) {
         auto inet = getValue<input_INET>(ctx);
-        emitValue<output_IP>(ctx, (typeof_IP)inet->localIP());
+        emitValue<output_IP>(ctx, (typeof_IP)inet.wifi->localIP());
     }
 }
