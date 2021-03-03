0f12a6f228f2458897d1d3577a70dd6f8a1d7ed2
Kirill Shumilov <brusher.designer@gmail.com>
2021-03-03 19:11:11 +0300

    fix(xod-client-browser): set navigation timeout to 60 seconds

--- packages/xod-client-browser/test-func/utils/getPage.js
+++ packages/xod-client-browser/test-func/utils/getPage.js
@@ -3,6 +3,8 @@ import { SERVER_URL } from '../server.config';
 export default async function getPage(browser) {
   const page = await browser.newPage();
 
+  await page.setDefaultNavigationTimeout(60000);
+
   await page.goto(SERVER_URL);
   await page.setViewport({ width: 1024, height: 768 });
   await page.waitForSelector('.Workarea');
